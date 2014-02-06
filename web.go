176d9b4ec53802f474211a7bf5330236a5415aba
Herman Schistad <hermansc@samfundet.no>
2014-02-06 21:42:33 +0100

    Initial commit

---
+++ web.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "net/http"
+    "html/template"
+)
+
+func indexPageHandler(w http.ResponseWriter, r *http.Request){
+    t, _ := template.ParseFiles("index.html")
+    d := make(map[string]string)
+    d["Who"] = "World"
+    t.Execute(w, d)
+}
+
+func main() {
+    http.HandleFunc("/", indexPageHandler)
+    http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("dist/"))))
+    http.ListenAndServe(":8080", nil)
+}

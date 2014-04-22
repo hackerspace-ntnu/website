0acc023ef5afeb56963c0bf467a6fa3dfe4405bd
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2014-04-22 23:01:58 +0200

    Changed default image from launch event

--- dist/js/gallery.js
+++ dist/js/gallery.js
@@ -20,6 +20,7 @@ $(document).ready(function(){
   Galleria.run('.galleria', {
     //dataSource: images,
     flickr: 'set:72157642546421854',
+    show: 11,
     flickrOptions: {
       description: true,
     },

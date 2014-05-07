3214a75118e74b290fec9305c4f60486fa3d2f67
Mathias Ose <mathias.ose@gmail.com>
2014-05-07 14:17:41 +0200

    status circle max width. crop header image. replace mazemap with image map. delay IRC load.

---
+++ dist/js/irc.js
@@ -0,0 +1,5 @@
+var kiwiIframe = '<iframe src="https://kiwiirc.com/client/irc.underworld.no/?nick=web|?&theme=mini#hackerspace"></iframe>';
+
+var loadKiwiIRC = function () {
+    $('#irc-container').html(kiwiIframe);
+};

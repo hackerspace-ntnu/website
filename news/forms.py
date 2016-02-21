8b77ef07bc95a13a97065c3c354e7f01acf3e5d0
ecuatox <a.tenstad@gmail.com>
2016-02-21 12:49:49 +0100

    Add date and time to EventEditForm

--- news/forms.py
+++ news/forms.py
@@ -3,9 +3,11 @@ from ckeditor.widgets import CKEditorWidget
 
 
 class EventEditForm(forms.Form):
-    event_id = forms.IntegerField();
+    event_id = forms.IntegerField(widget=forms.HiddenInput())
     ingress_content = forms.CharField(widget=CKEditorWidget(), label='')
     main_content = forms.CharField(widget=CKEditorWidget(), label='')
+    time = forms.CharField(widget=forms.HiddenInput())
+    date = forms.CharField(widget=forms.HiddenInput())
 
 
 class ArticleEditForm(forms.Form):

980b91fa74278cda2aed9c8e48fc6d4b8d7636c7
ecuatox <a.tenstad@gmail.com>
2016-02-21 18:50:39 +0100

    Show ingress and article labels in event_edit.html

--- news/forms.py
+++ news/forms.py
@@ -4,8 +4,8 @@ from ckeditor.widgets import CKEditorWidget
 
 class EventEditForm(forms.Form):
     event_id = forms.IntegerField(widget=forms.HiddenInput())
-    ingress_content = forms.CharField(widget=CKEditorWidget(), label='')
-    main_content = forms.CharField(widget=CKEditorWidget(), label='')
+    ingress_content = forms.CharField(widget=CKEditorWidget(), label='Ingress')
+    main_content = forms.CharField(widget=CKEditorWidget(), label='Article')
     time = forms.CharField(widget=forms.HiddenInput())
     date = forms.CharField(widget=forms.HiddenInput())
     place = forms.CharField(max_length=100, label='Location')

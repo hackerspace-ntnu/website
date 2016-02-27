e8eff8ee0d8b69fb6eaac2c85948759149d067a8
ecuatox <a.tenstad@gmail.com>
2016-02-27 18:43:00 +0100

    Remove max_length in Upload

--- news/models.py
+++ news/models.py
@@ -52,7 +52,7 @@ class Upload(models.Model):
     title = models.CharField(max_length=100, verbose_name='Filename')
     time = models.DateTimeField()
     file = models.FileField(upload_to='uploads')
-    number = models.IntegerField(max_length=100)
+    number = models.IntegerField()
 
     def __str__(self):
         return self.title

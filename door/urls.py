9d8dfc0581d3bef8da89271650a546a70a477fb3
ecuatox <a.tenstad@gmail.com>
2016-03-03 16:42:48 +0100

    Add get_json to door urls

--- door/urls.py
+++ door/urls.py
@@ -6,5 +6,6 @@ from website.settings import DOOR_KEY
 urlpatterns = [
     url(r'^$', views.door_post, name='door_post'),
     url(r'^get_status/', views.get_status, name='get_status'),
+    url(r'^get_json/', views.get_json, name='get_json'),
     url(r'^door-data/', views.door_data, name='door_data'),
 ]

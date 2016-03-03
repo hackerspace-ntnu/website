c04dfea61228d62a151822f8ebbdc9bfc07ec999
ecuatox <a.tenstad@gmail.com>
2016-03-03 12:46:10 +0100

    Add data to admin view

--- door/admin.py
+++ door/admin.py
@@ -7,10 +7,26 @@ class DoorStatusAdmin(admin.ModelAdmin):
         ('Status', {
             'fields': [
                 'name',
+                'datetime',
                 'status',
             ]
         })
     ]
     search_fields = [
         'title'
+    ]
+
+@admin.register(OpenData)
+class OpenDataAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Data', {
+            'fields': [
+                'opened',
+                'closed',
+                'data',
+            ]
+        })
+    ]
+    search_fields = [
+        'opened'
     ]

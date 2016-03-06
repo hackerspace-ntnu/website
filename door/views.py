3a4b4fa5db3d19582fc86930ee902ede5f40c11c
ecuatox <a.tenstad@gmail.com>
2016-03-06 12:39:55 +0100

    Only update plot db every 3 min
    And when door status is changed

--- door/views.py
+++ door/views.py
@@ -92,52 +92,64 @@ def door_data(request):
     return render(request, 'door_data.html', context)
 
 def door_chart(request):
-    Point.objects.all().delete()
     try:
         door_obj = DoorStatus.objects.get(name='hackerspace')
     except DoorStatus.DoesNotExist:
         door_obj = DoorStatus(name='hackerspace', status=True, datetime=timezone.now())
 
-    start = OpenData.objects.all()[0].opened
-    start = start - timedelta(minutes=start.minute % 10, seconds=start.second, microseconds=start.microsecond)
-    end = timezone.now()
-    end = end - timedelta(minutes=end.minute % 10, seconds=end.second, microseconds=end.microsecond)
+    try:
+        plot_obj = DoorStatus.objects.get(name='plotchart')
+    except DoorStatus.DoesNotExist:
+        plot_obj = DoorStatus(name='plotchart', status=not door_obj.status, datetime=timezone.now())
+        plot_obj.save()
 
-    index = 0
-    status = 0
-    statusAdded = False
-    tooltipString = ["Closed", "Open"]
-    for seconds in range(0, int((end-start).total_seconds()), 600):
-        if index < len(OpenData.objects.all()):
-            if status == 0 and seconds > (OpenData.objects.all()[index].opened-start).total_seconds():
-                entry = Point(datetime=OpenData.objects.all()[index].opened, status=status, tooltip="Door opened " + str(OpenData.objects.all()[index].opened))
-                entry.save()
-                status = 1
-                entry = Point(datetime=OpenData.objects.all()[index].opened, status=status, tooltip="Door opened " + str(OpenData.objects.all()[index].opened))
-                entry.save()
-            if status == 1 and seconds > (OpenData.objects.all()[index].closed-start).total_seconds():
-                entry = Point(datetime=OpenData.objects.all()[index].closed, status=status, tooltip="Door closed " + str(OpenData.objects.all()[index].closed))
-                entry.save()
-                status = 0
-                entry = Point(datetime=OpenData.objects.all()[index].closed, status=status, tooltip="Door closed " + str(OpenData.objects.all()[index].closed))
-                entry.save()
-                index += 1
-        if seconds > (door_obj.datetime-start).total_seconds() and not statusAdded:
-            statusAdded = True
-            if door_obj.status:
-                entry = Point(datetime=door_obj.datetime, status=0, tooltip="Door opened "+str(door_obj.datetime))
-                entry.save()
-                entry = Point(datetime=door_obj.datetime, status=1, tooltip="Door opened "+str(door_obj.datetime))
-                entry.save()
-                status = 1
-        entry = Point(datetime=start + timedelta(0, seconds), status=status, tooltip=str(tooltipString[status]))
-        entry.save()
-    if door_obj.status:
-        entry = Point(datetime=timezone.now(), status=1, tooltip="Currently open")
-        entry.save()
-    else:
-        entry = Point(datetime=timezone.now(), status=1, tooltip="Currently closed")
-        entry.save()
+    if (timezone.now() - plot_obj.datetime).total_seconds() > 180 or plot_obj.status != door_obj.status:
+        Point.objects.all().delete()
+
+        start = OpenData.objects.all()[0].opened
+        start = start - timedelta(minutes=start.minute % 10, seconds=start.second, microseconds=start.microsecond)
+        end = timezone.now()
+        end = end - timedelta(minutes=end.minute % 10, seconds=end.second, microseconds=end.microsecond)
+
+        index = 0
+        status = 0
+        statusAdded = False
+        tooltipString = ["Closed", "Open"]
+        for seconds in range(0, int((end-start).total_seconds()), 600):
+            if index < len(OpenData.objects.all()):
+                if status == 0 and seconds > (OpenData.objects.all()[index].opened-start).total_seconds():
+                    entry = Point(datetime=OpenData.objects.all()[index].opened, status=status, tooltip="Door opened " + str(OpenData.objects.all()[index].opened))
+                    entry.save()
+                    status = 1
+                    entry = Point(datetime=OpenData.objects.all()[index].opened, status=status, tooltip="Door opened " + str(OpenData.objects.all()[index].opened))
+                    entry.save()
+                if status == 1 and seconds > (OpenData.objects.all()[index].closed-start).total_seconds():
+                    entry = Point(datetime=OpenData.objects.all()[index].closed, status=status, tooltip="Door closed " + str(OpenData.objects.all()[index].closed))
+                    entry.save()
+                    status = 0
+                    entry = Point(datetime=OpenData.objects.all()[index].closed, status=status, tooltip="Door closed " + str(OpenData.objects.all()[index].closed))
+                    entry.save()
+                    index += 1
+            if seconds > (door_obj.datetime-start).total_seconds() and not statusAdded:
+                statusAdded = True
+                if door_obj.status:
+                    entry = Point(datetime=door_obj.datetime, status=0, tooltip="Door opened "+str(door_obj.datetime))
+                    entry.save()
+                    entry = Point(datetime=door_obj.datetime, status=1, tooltip="Door opened "+str(door_obj.datetime))
+                    entry.save()
+                    status = 1
+            entry = Point(datetime=start + timedelta(0, seconds), status=status, tooltip=str(tooltipString[status]))
+            entry.save()
+        if door_obj.status:
+            entry = Point(datetime=timezone.now(), status=1, tooltip="Currently open")
+            entry.save()
+        else:
+            entry = Point(datetime=timezone.now(), status=0, tooltip="Currently closed")
+            entry.save()
+
+        plot_obj.datetime = timezone.now()
+        plot_obj.status = door_obj.status
+        plot_obj.save()
 
     context = {
         'plot_list': Point.objects.all(),

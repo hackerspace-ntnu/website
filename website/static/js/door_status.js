da737ac58a1bd3cdf7e5a1f560961b1963350986
ecuatox <a.tenstad@gmail.com>
2016-04-12 23:09:23 +0200

    Fix door refresh

--- website/static/js/door_status.js
+++ website/static/js/door_status.js
@@ -1,21 +1,24 @@
 $(document).ready(function() {
     setTimeout(function(){get_data()}, 30000);
-    function get_data() {
-        $.ajax({
-            "type": "POST",
-            "url": "/door/get_status/",
-            "success": function(data) {
-              change_status(data);
-            },
-        });
-        setTimeout(function(){get_data()}, 30000);
-    }
 });
 
+function get_data() {
+    $.ajax({
+        "type": "POST",
+        "url": "/door/get_status/",
+        "success": function(data) {
+          change_status(data);
+        },
+    });
+    setTimeout(function(){get_data()}, 30000);
+
+}
+
 function change_status(status) {
+    console.log(status);
     if (status == 'True') {
-        $("#status").css("background-image","url(/static/img/Logo_green.png)");
+        $("#statusImg").attr("src","/static/img/Logo_green.png");
     } else if (status == 'False') {
-        $("#status").css("background-image","url(/static/img/Logo_white.png)");
+        $("#statusImg").attr("src","/static/img/Logo_white.png");
     }
 }

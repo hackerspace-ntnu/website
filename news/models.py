99be66b9dc041a90474ac835ff5547b39413e290
martsime <simensen94@gmail.com>
2016-05-06 18:33:18 +0200

    Add sorting to events. Show events nearest in future first

--- news/models.py
+++ news/models.py
@@ -16,27 +16,27 @@ class Article(models.Model):
 
     class Meta:
         app_label = 'news'
+        ordering = ('-pub_date',)
 
 
 class Event(models.Model):
     title = models.CharField(max_length=100, verbose_name='Title')
     main_content = RichTextUploadingField(blank=True)
     ingress_content = RichTextUploadingField(blank=True)
+    pub_date = models.DateTimeField('Publication date', default=timezone.now)
+    thumbnail = models.CharField(max_length=200, blank=True)
 
     time_start = models.DateTimeField('Start time')
     time_end = models.DateTimeField('End time')
     place = models.CharField(max_length=100, verbose_name='Place', blank=True)
     place_href = models.CharField(max_length=200, verbose_name='Place URL', blank=True)
 
-    pub_date = models.DateTimeField('Publication date', default=timezone.now)
-
-    thumbnail = models.CharField(max_length=200, blank=True)
-
     def __str__(self):
         return self.title
 
     class Meta:
         app_label = 'news'
+        ordering = ("-time_start",)
 
 
 class Upload(models.Model):
--- website/views.py
+++ website/views.py
@@ -4,11 +4,17 @@ from door.models import DoorStatus
 from authentication.forms import LoginForm
 from django_user_agents.utils import get_user_agent
 from local_settings import DEBUG
+from datetime import datetime
 
 
 def index(request):
-    event_list = Event.objects.order_by('-time_start')[:3]
-    article_list = Article.objects.order_by('-pub_date')[:3]
+
+    number_of_news = 4
+
+    # Sorts the news to show the events nearest in future and then fill in with the newest articles
+    event_list = Event.objects.filter(time_start__gte=datetime.now())[0:number_of_news:-1]
+    article_list = Article.objects.order_by('-pub_date')[0:number_of_news - len(event_list)]
+
     try:
         door_status = DoorStatus.objects.get(name='hackerspace').status
     except DoorStatus.DoesNotExist:

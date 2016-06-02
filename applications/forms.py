3735c0c8a7df80d665621a6641669f0b38b20bcd
martsime <simensen94@gmail.com>
2016-06-02 17:17:36 +0200

    Change the whole application app

--- applications/forms.py
+++ applications/forms.py
@@ -3,15 +3,21 @@ from .models import Application
 
 
 class ApplicationForm(ModelForm):
+
+    year_choices = [choice[1] for choice in Application.YEAR_CHOICES]
+    group_choices = [choice[1] for choice in Application.GROUP_CHOICES]
+
     class Meta:
         model = Application
         fields = ['name',
                   'email',
                   'phone',
-                  'study_program',
+                  'study',
+                  'group_choice',
                   'year',
-                  'knowledge_of_hackerspace',
-                  'choice_of_group',
+                  'knowledge_of_hs',
                   'about',
                   'application_text',
-        ]
+                  ]
+
+

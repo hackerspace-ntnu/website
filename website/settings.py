bbe8890386c8bbd975e059273c7ea0f19ad404d7
martsime <simensen94@gmail.com>
2016-08-28 20:53:31 +0200

    Clean up settings file

---
+++ website/settings.py
@@ -0,0 +1,199 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
+from os import path as os_path
+import os
+import sys
+
+#################################
+# General 						#
+#################################
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SECRET_KEY = "DefaultSecretKey"
+ROOT_URLCONF = 'website.urls'
+WSGI_APPLICATION = 'website.wsgi.application'
+SITE_ID = 1
+
+ADMINS = (
+	('devops', 'hackerspace-dev@idi.ntnu.no'),
+)
+
+# Set some variables if DEBUG == False
+if os.environ.get('DEBUG') == 'False':
+	DEBUG = False
+	SECRET_KEY = os.environ.get('SECRET_KEY')
+	DOOR_KEY = os.environ.get('DOOR_KEY')
+	ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+else:
+	DEBUG = True
+
+#################################
+# Installed apps                #
+#################################
+
+INSTALLED_APPS = [
+	'django.contrib.humanize',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.sites',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
+	'django.contrib.admin',
+	'django.contrib.admindocs',
+	'website',
+	'applications',
+	'news',
+	'door',
+	'ckeditor',
+	'ckeditor_uploader',
+	'sekizai',
+	'sorl.thumbnail',
+	'django_nyt',
+	'wiki',
+	'wiki.plugins.macros',
+	'wiki.plugins.help',
+	'wiki.plugins.links',
+	'wiki.plugins.images',
+	'wiki.plugins.attachments',
+	'wiki.plugins.notifications',
+	'mptt',
+	'authentication',
+]
+
+#################################
+# Database                      #
+#################################
+
+if DEBUG:
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.sqlite3',
+			'NAME': 'database',
+		}
+	}
+else:
+	DATABASES = {
+		'default': {
+			'ENGINE': 'django.db.backends.postgresql_psycopg2',
+			'NAME': os.environ.get('POSTGRES_USER'),
+			'USER': os.environ.get('POSTGRES_USER'),
+			'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+			'HOST': 'database',
+			'PORT': '5432',
+		}
+	}
+
+#################################
+# Templates                     #
+#################################
+
+TEMPLATES = [
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'APP_DIRS': True,
+		'DIRS': (os.path.join(BASE_DIR, 'templates/'),),
+		'OPTIONS': {
+			'context_processors': [
+				"django.contrib.auth.context_processors.auth",
+				"django.template.context_processors.debug",
+				"django.template.context_processors.i18n",
+				"django.template.context_processors.media",
+				"django.template.context_processors.request",
+				"django.template.context_processors.static",
+				"django.template.context_processors.tz",
+				"django.contrib.messages.context_processors.messages",
+				"sekizai.context_processors.sekizai",
+			],
+			'debug': DEBUG,
+		}
+	},
+]
+
+#################################
+# Security                      #
+#################################
+
+MIDDLEWARE_CLASSES = [
+	'django.middleware.common.CommonMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+]
+
+#################################
+# Static                        #
+#################################
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+if not DEBUG:
+	STATIC_ROOT = '/code/static'
+	MEDIA_ROOT = '/code/media'
+
+CKEDITOR_UPLOAD_PATH = os_path.join(BASE_DIR, 'media/uploads')
+CKEDITOR_BROWSE_SHOW_DIRS = True
+CKEDITOR_UPLOAD_SLUGIFY_FILENAME = False
+CKEDITOR_RESTRICT_BY_USER = False
+CKEDITOR_BROWSE_SHOW_DIRS = False
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static', ),)
+
+STATICFILES_FINDERS = (
+	'django.contrib.staticfiles.finders.FileSystemFinder',
+	'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+	'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+#################################
+# Email                         #
+#################################
+
+DEFAULT_FROM_MAIL = 'hackerspace-dev@idi.ntnu.no'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = '173.194.71.108'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+#################################
+# Internalization               #
+#################################
+
+TIME_ZONE = 'Europe/Oslo'
+
+LANGUAGE_CODE = 'nb'
+
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+#################################
+# Logging                       #
+#################################
+
+LOGGING = {
+	'version': 1,
+	'disable_existing_loggers': False,
+	'filters': {
+		'require_debug_false': {
+			'()': 'django.utils.log.RequireDebugFalse'
+		}
+	},
+	'handlers': {
+		'mail_admins': {
+			'level': 'ERROR',
+			'filters': ['require_debug_false'],
+			'class': 'django.utils.log.AdminEmailHandler'
+		}
+	},
+	'loggers': {
+		'django.request': {
+			'handlers': ['mail_admins'],
+			'level': 'ERROR',
+			'propagate': True,
+		},
+	}
+}
--- wiki/conf/settings.py
+++ wiki/conf/settings.py
@@ -6,6 +6,9 @@ from django.core.urlresolvers import reverse_lazy
 from django.utils.translation import ugettext_lazy as _
 import django
 
+WIKI_ANONYMOUS_WRITE = False
+WIKI_ANONYMOUS_CREATE = False
+
 # Should urls be case sensitive?
 URL_CASE_SENSITIVE = getattr(django_settings, 'WIKI_URL_CASE_SENSITIVE', False)
 
@@ -16,22 +19,22 @@ WIKI_LANGUAGE = 'markdown'
 # The editor class to use -- maybe a 3rd party or your own...? You can always
 # extend the built-in editor and customize it....
 EDITOR = getattr(
-    django_settings,
-    'WIKI_EDITOR',
-    'wiki.editors.markitup.MarkItUp')
+	django_settings,
+	'WIKI_EDITOR',
+	'wiki.editors.markitup.MarkItUp')
 
 MARKDOWN_KWARGS = {
-    'extensions': [
-        'footnotes',
-        'attr_list',
-        'extra',
-        'codehilite',
-        'sane_lists',
-    ],
-    'safe_mode': 'replace',
-    'extension_configs': {
-        'toc': {
-            'title': _('Table of Contents')}},
+	'extensions': [
+		'footnotes',
+		'attr_list',
+		'extra',
+		'codehilite',
+		'sane_lists',
+	],
+	'safe_mode': 'replace',
+	'extension_configs': {
+		'toc': {
+			'title': _('Table of Contents')}},
 }
 MARKDOWN_KWARGS.update(getattr(django_settings, 'WIKI_MARKDOWN_KWARGS', {}))
 
@@ -39,16 +42,16 @@ MARKDOWN_KWARGS.update(getattr(django_settings, 'WIKI_MARKDOWN_KWARGS', {}))
 # URLPath of that article are moved to lost and found. They keep their permissions
 # and all their content.
 LOST_AND_FOUND_SLUG = getattr(
-    django_settings,
-    'WIKI_LOST_AND_FOUND_SLUG',
-    'lost-and-found')
+	django_settings,
+	'WIKI_LOST_AND_FOUND_SLUG',
+	'lost-and-found')
 
 # When True, this blocks new slugs that resolve to non-wiki views, stopping
 # users creating articles that conflict with overlapping URLs from other apps.
 CHECK_SLUG_URL_AVAILABLE = getattr(
-    django_settings,
-    'WIKI_CHECK_SLUG_URL_AVAILABLE',
-    True)
+	django_settings,
+	'WIKI_CHECK_SLUG_URL_AVAILABLE',
+	True)
 
 # Do we want to log IPs?
 LOG_IPS_ANONYMOUS = getattr(django_settings, 'WIKI_LOG_IPS_ANONYMOUS', True)
@@ -85,9 +88,9 @@ CAN_ASSIGN_OWNER = getattr(django_settings, 'WIKI_ASSIGN_OWNER', None)
 # A function returning True/False if a user has permission to change
 # read/write access for groups and others
 CAN_CHANGE_PERMISSIONS = getattr(
-    django_settings,
-    'WIKI_CAN_CHANGE_PERMISSIONS',
-    None)
+	django_settings,
+	'WIKI_CAN_CHANGE_PERMISSIONS',
+	None)
 
 # Specifies if a user has access to soft deletion of articles
 CAN_DELETE = getattr(django_settings, 'WIKI_CAN_DELETE', None)
@@ -110,9 +113,9 @@ ANONYMOUS_WRITE = getattr(django_settings, 'WIKI_ANONYMOUS_WRITE', False)
 # Globally enable create access for anonymous users
 # Defaults to ANONYMOUS_WRITE.
 ANONYMOUS_CREATE = getattr(
-    django_settings,
-    'WIKI_ANONYMOUS_CREATE',
-    ANONYMOUS_WRITE)
+	django_settings,
+	'WIKI_ANONYMOUS_CREATE',
+	ANONYMOUS_WRITE)
 
 # Default setting to allow anonymous users upload access (used in
 # plugins.attachments and plugins.images).
@@ -124,15 +127,15 @@ ACCOUNT_HANDLING = getattr(django_settings, 'WIKI_ACCOUNT_HANDLING', True)
 # Signup allowed? If it's not allowed, logged in superusers can still access
 # the signup page to create new users.
 ACCOUNT_SIGNUP_ALLOWED = ACCOUNT_HANDLING and getattr(
-    django_settings, 'WIKI_ACCOUNT_SIGNUP_ALLOWED', True
+	django_settings, 'WIKI_ACCOUNT_SIGNUP_ALLOWED', True
 )
 
 if ACCOUNT_HANDLING:
-    LOGIN_URL = reverse_lazy("wiki:login")
-    LOGOUT_URL = reverse_lazy("wiki:logout")
+	LOGIN_URL = reverse_lazy("wiki:login")
+	LOGOUT_URL = reverse_lazy("wiki:logout")
 else:
-    LOGIN_URL = getattr(django_settings, "LOGIN_URL", "/")
-    LOGOUT_URL = getattr(django_settings, "LOGOUT_URL", "/")
+	LOGIN_URL = getattr(django_settings, "LOGIN_URL", "/")
+	LOGOUT_URL = getattr(django_settings, "LOGOUT_URL", "/")
 
 ##################
 # OTHER SETTINGS #
@@ -144,27 +147,27 @@ else:
 SHOW_MAX_CHILDREN = getattr(django_settings, 'WIKI_SHOW_MAX_CHILDREN', 20)
 
 USE_BOOTSTRAP_SELECT_WIDGET = getattr(
-    django_settings,
-    'WIKI_USE_BOOTSTRAP_SELECT_WIDGET',
-    True)
+	django_settings,
+	'WIKI_USE_BOOTSTRAP_SELECT_WIDGET',
+	True)
 
 #: dottedname of class used to construct urlpatterns for wiki.
 #:
 #: Default is wiki.urls.WikiURLPatterns. To customize urls or view handlers,
 #: you can derive from this.
 URL_CONFIG_CLASS = getattr(
-    django_settings,
-    'WIKI_URL_CONFIG_CLASS',
-    'wiki.urls.WikiURLPatterns')
+	django_settings,
+	'WIKI_URL_CONFIG_CLASS',
+	'wiki.urls.WikiURLPatterns')
 
 # Search view - dotted path denoting where the search view Class is located
 SEARCH_VIEW = getattr(
-    django_settings,
-    'WIKI_SEARCH_VIEW',
-    'wiki.views.article.SearchView'
-    if 'wiki.plugins.haystack' not in django_settings.INSTALLED_APPS
-    else
-    'wiki.plugins.haystack.views.HaystackSearchView'
+	django_settings,
+	'WIKI_SEARCH_VIEW',
+	'wiki.views.article.SearchView'
+	if 'wiki.plugins.haystack' not in django_settings.INSTALLED_APPS
+	else
+	'wiki.plugins.haystack.views.HaystackSearchView'
 )
 
 # Seconds of timeout before renewing article cache. Articles are automatically
@@ -175,9 +178,9 @@ CACHE_TIMEOUT = getattr(django_settings, 'WIKI_CACHE_TIMEOUT', 600)
 # Choose the Group model to use. Defaults to django's auth.Group
 # This requires `django.apps` which was introduced in Django 1.7.
 if django.VERSION < (1, 7):
-    GROUP_MODEL = 'auth.Group'
+	GROUP_MODEL = 'auth.Group'
 else:
-    GROUP_MODEL = getattr(django_settings, 'WIKI_GROUP_MODEL', 'auth.Group')
+	GROUP_MODEL = getattr(django_settings, 'WIKI_GROUP_MODEL', 'auth.Group')
 
 ###################
 # SPAM PROTECTION #
@@ -188,38 +191,39 @@ REVISIONS_PER_HOUR = getattr(django_settings, 'WIKI_REVISIONS_PER_HOUR', 60)
 
 # Maximum allowed revisions per minute for any given user or IP
 REVISIONS_PER_MINUTES = getattr(
-    django_settings,
-    'WIKI_REVISIONS_PER_MINUTES',
-    5)
+	django_settings,
+	'WIKI_REVISIONS_PER_MINUTES',
+	5)
 
 # Maximum allowed revisions per hour for any given user or IP
 REVISIONS_PER_HOUR_ANONYMOUS = getattr(
-    django_settings,
-    'WIKI_REVISIONS_PER_HOUR_ANONYMOUS',
-    10)
+	django_settings,
+	'WIKI_REVISIONS_PER_HOUR_ANONYMOUS',
+	10)
 
 # Maximum allowed revisions per hour for any given user or IP
 REVISIONS_PER_MINUTES_ANONYMOUS = getattr(
-    django_settings,
-    'WIKI_REVISIONS_PER_MINUTES_ANONYMOUS',
-    2)
+	django_settings,
+	'WIKI_REVISIONS_PER_MINUTES_ANONYMOUS',
+	2)
 
 # Number of minutes for looking up REVISIONS_PER_MINUTES and
 # REVISIONS_PER_MINUTES_ANONYMOUS
 REVISIONS_MINUTES_LOOKBACK = getattr(
-    django_settings,
-    'WIKI_REVISIONS_MINUTES_LOOKBACK',
-    2)
+	django_settings,
+	'WIKI_REVISIONS_MINUTES_LOOKBACK',
+	2)
 
 ###########
 # STORAGE #
 ###########
 
 from django.core.files.storage import default_storage
+
 STORAGE_BACKEND = getattr(
-    django_settings,
-    'WIKI_STORAGE_BACKEND',
-    default_storage)
+	django_settings,
+	'WIKI_STORAGE_BACKEND',
+	default_storage)
 
 USE_SENDFILE = getattr(django_settings, 'WIKI_ATTACHMENTS_USE_SENDFILE', False)
 
@@ -232,6 +236,6 @@ MAX_REVISIONS = getattr(django_settings, 'WIKI_MAX_REVISIONS', 100)
 
 # Maximum age of revisions in days, 0=unlimited
 MAX_REVISION_AGE = getattr(
-    django_settings, 'WIKI_MAX_REVISION_AGE', 
-    getattr(django_settings, 'MAX_REVISION_AGE', 365)
+	django_settings, 'WIKI_MAX_REVISION_AGE',
+	getattr(django_settings, 'MAX_REVISION_AGE', 365)
 )

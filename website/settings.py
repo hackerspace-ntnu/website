db322752b275b8aaa2d139292d5dedbc12c68525
martsime <simensen94@gmail.com>
2016-09-03 16:42:10 +0200

    Remove mail logger

--- website/settings.py
+++ website/settings.py
@@ -180,20 +180,5 @@ LOGGING = {
 		'require_debug_false': {
 			'()': 'django.utils.log.RequireDebugFalse'
 		}
-	},
-	'handlers': {
-		'mail_admins': {
-			'level': 'ERROR',
-			'filters': ['require_debug_false'],
-			'class': 'django.utils.log.AdminEmailHandler',
-			'include_html': True,
-		}
-	},
-	'loggers': {
-		'django.request': {
-			'handlers': ['mail_admins'],
-			'level': 'ERROR',
-			'propagate': True,
-		},
 	}
 }

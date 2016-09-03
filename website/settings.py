3543213da03a6cd02f6bf6db31d598aeb57c8b07
martsime <simensen94@gmail.com>
2016-09-03 02:04:42 +0200

    Fix formatting on erorr emails

--- website/settings.py
+++ website/settings.py
@@ -187,7 +187,8 @@ LOGGING = {
 		'mail_admins': {
 			'level': 'ERROR',
 			'filters': ['require_debug_false'],
-			'class': 'django.utils.log.AdminEmailHandler'
+			'class': 'django.utils.log.AdminEmailHandler',
+			'include_html': True,
 		}
 	},
 	'loggers': {

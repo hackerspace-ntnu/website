dcb5e0954164521da5a41eea2b1af4df9fc4156e
Karl August Hoivik <gussefant@gmail.com>
2016-10-25 15:08:48 +0200

    Add hostnames from file

--- rpi/views.py
+++ rpi/views.py
@@ -6,10 +6,29 @@ import datetime
 from .models import RaspberryPi
 from random import choice
 
-names = {
-    "ac:7b:a1:56:ae:5a":"Lovelace"
-}
-availNames = ["Ariel","Jane"]
+availNames = []
+def reserveName(name):
+    global availNames
+    availNames.remove(name)
+    with open("availnames.txt",mode="w") as f:
+        for n in availNames:
+            f.write(n+"\n")
+    f.close()
+def refresh_rpis():
+    global availNames
+    data=""
+    """
+    with open("RPi.json",mode="r") as f:
+        for line in f:
+            data+=line
+    names = json.loads(data)
+    """
+    availNames = []
+    with open("availnames.txt",mode="r") as f:
+        for line in f:
+            availNames.append(line.strip())
+    f.close()
+
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
@@ -24,6 +43,8 @@ def getPi(mac):
     else:
         return rpis[0]
 def addrpi(request):
+    if len(availNames)==0:
+        refresh_rpis()
     if request.POST:
         mac = request.POST.get('mac_address','')
         ip = get_client_ip(request)
@@ -31,20 +52,30 @@ def addrpi(request):
         time = datetime.datetime.strftime(datetime.datetime.now(),format="%Y-%m-%d %H:%M")
         if getPi(mac) is None:
             RaspberryPi.objects.create(name=hostname,ip=ip,mac=mac,lastSeen=time)
+            reserveName(hostname)
             return HttpResponse(hostname)
         else:
             print("{} already exists".format(mac))
             return HttpResponse("")
 def lifesign(request):
+    if len(availNames)==0:
+        refresh_rpis()
     if request.POST:
         mac = request.POST.get('mac_address','')
-        ip= get_client_ip(request)
-        hostname = names[mac]
+        reported_hostname = request.POST.get('hostname','')
         host_rpi = getPi(mac)
-        time = datetime.datetime.strftime(datetime.datetime.now(),format="%Y-%m-%d %H:%M")
-        host_rpi.update(name=hostname,ip=ip,mac=mac,lastSeen=time)
-    return HttpResponse("Hello, "+hostname)
-
+        if host_rpi is not None:
+            ip= get_client_ip(request)
+            time = datetime.datetime.strftime(datetime.datetime.now(),format="%Y-%m-%d %H:%M")
+            hostname = host_rpi.name
+            if reported_hostname != hostname:
+                host_rpi.update(name=hostname)
+            host_rpi.update(ip=ip,lastSeen=time)
+            return HttpResponse("Hello, "+hostname)
+        else:
+            return HttpResponse("You're new here, arent'cha?")
+    else:
+        return HttpResponse("We only serve POST requests here")
 def index(request):
     all_rpis = RaspberryPi.objects.order_by('-lastSeen')[:5]
     template = loader.get_template('rpis.html')

b2539f4a2b93c6afe9262cc395caca5ae7eb1e4a
sigmundholm <sigmundhol@hotmail.com>
2016-11-08 08:25:45 +0100

    make add_item and add_tag changeable, add view for registered and tag_detail

--- inventory/views.py
+++ inventory/views.py
@@ -8,10 +8,16 @@ from .models import Tag, Item, Loan
 from .forms import ItemForm, LoanForm, TagForm
 
 
+# TODO tekst på knapper må midtstilles
+# TODO knapper må flyttes til bestemt punkt på skjermen
+# TODO lage søkeside
+# TODO mulig å bruke QR-kode for merking av gjenstander, må føre til item sin detail-side
+
 def index(request):
     items = Item.objects.all()
     # items.sort(key=lambda a: a.tags.get(pk=1))
-    item_dict = {}
+    item_dict = {}  # TODO vurdere å bytte ut med en sorted dict for å kunne vise kategoriene sortert,
+    # evt legge kategorier i en sortert liste
     no_category = []
     for item in items:
         if not item.tags.all():
@@ -35,61 +41,96 @@ def index(request):
 
 def detail(request, item_id):
     item = get_object_or_404(Item, pk=item_id)
-
-    context = {
-        'item': Item.objects.get(pk=item_id)
-    }
-    return render(request, 'inventory/detail.html', context)
+    return render(request, 'inventory/detail.html', {'item': item})
 
 
-# @login_required
-def add_item(request, id=None):
+# @login_required  # TODO tilgjenelighet må endres mtp om man er innlogget eller ikke
+def add_item(request, item_id=0):
+    message = "Legg til en ny gjenstand"
+    button_message = "registrer"
 
     if request.method == 'POST':
         form = ItemForm(request.POST)
-        if id:
-            # TODO eksisterende id skal bare endres, ikke lag ny
-            item = Item.objects.get(pk=id)
-            item.name = form.changed_data['name']
-
-
-            pass
-
         if form.is_valid():
-            name = form.cleaned_data['name']
-            description = form.cleaned_data['descritption']
-            quantity = form.cleaned_data['quantity']
-            tags = form.cleaned_data['tags']
-
-            item = Item(name=name, quantity=quantity, description=description)
-            item.save()
+            if item_id != '0':  # existing item to be changed
+                item = Item.objects.get(pk=item_id)
+                item.name = form.cleaned_data['name']
+                item.description = form.cleaned_data['description']
+                item.quantity = form.cleaned_data['quantity']
+                item.tags = form.cleaned_data['tags']
+                item.save()
+            else:  # create new item from form
+                name = form.cleaned_data['name']
+                description = form.cleaned_data['description']
+                quantity = form.cleaned_data['quantity']
+                tags = form.cleaned_data['tags']
+                item = Item(name=name, description=description, quantity=quantity)
+                item.save()
 
             # TODO legge til at en liten melding vises øverst når man laster inn neste side, se: "the messages framwork"
-
             return HttpResponseRedirect(reverse('inventory:registered'))
     else:
         form = ItemForm()
-        if id:
-            # TODO eksisterende item skal endres, form må fylles med data
-            pass
 
-    return render(request, 'inventory/add_item.html', {'form': form})
+        if item_id:
+            message = "Endre gjenstand"
+            button_message = "endre"
+
+            item = Item.objects.get(pk=item_id)
+            initial = {
+                'name': item.name,
+                'description': item.description,
+                'quantity': item.quantity,
+                'tags': item.tags,
+            }
+            form = ItemForm(initial=initial)
+
+    context = {
+        'form': form,
+        'message': message,
+        'button_message': button_message,
+        'item_id': item_id,
+    }
+    return render(request, 'inventory/add_item.html', context)
 
 
-def add_tag(request):
+# @login_required  # TODO tilgjenelighet må endres mtp om man er innlogget eller ikke
+def add_tag(request, tag_id=0):
+    message = "Legg til ny tag"
+    button_message = "Legg til"
+
     if request.method == 'POST':
         form = TagForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            tag = Tag(name=name)
-            tag.save()
+            if tag_id != '0':  # form sender streng tilbake, selv om den passes som inten 0 i context
+                tag = Tag.objects.get(pk=tag_id)
+                tag.name = form.cleaned_data['name']
+                tag.save()
+            else:
+                name = form.cleaned_data['name']
+                tag = Tag(name=name)
+                tag.save()
+
+            # TODO legge til at en liten melding vises øverst når man laster inn neste side, se: "the messages framwork"
             return HttpResponseRedirect(reverse('inventory:registered'))
     else:
         form = TagForm()
+        if tag_id:
+            message = "Endre tag"
+            button_message = "Endre"
+            tag = Tag.objects.get(pk=tag_id)
+            form = TagForm(initial={'name': tag.name})
 
-    return render(request, 'inventory/add_tag.html', {'form': form})
+    context = {
+        'form': form,
+        'message': message,
+        'button_message': button_message,
+        'tag_id': tag_id,
+    }
+    return render(request, 'inventory/add_tag.html', context)
 
 
+# @login_required  # TODO tilgjenelighet må endres mtp om man er innlogget eller ikke
 def loan(request):
     if request.method == 'POST':
         form = LoanForm(request.POST)
@@ -109,10 +150,21 @@ def loan(request):
     return HttpResponse(render(request, 'inventory/loan.html', {'form': form}))
 
 
-def change_item(request, item_id):
-    return HttpResponse("endre objekt")
-
-
 def registered(request):
     name = "NAME"
-    return HttpResponse("Din " + name + " ble registert." + '<a href="/inventory/">link</a> ')
+    # TODO mulig å heller redirecte til index, og ha en toast som sier at item.name er registrert, må da finne en
+    # måte å vise denne toasten selv om man kommer til ny side
+    beskjed = '<p>Dette er en midlertidig side fra view registered, som bør erstattes med en toast</p>'
+
+    return render(request, 'inventory/registered.html', {'type': name})
+
+
+def tag_detail(request, tag_id):
+    # TODO lage bedre oversikt over items, feks collapsible
+    tag = get_object_or_404(Tag, pk=tag_id)
+    related_items = tag.item_set.all()
+    context = {
+        'tag': tag,
+        'related_items': related_items,
+    }
+    return render(request, 'inventory/tag_detail.html', context)

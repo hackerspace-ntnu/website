ee3df017cf0ad4db1a26a7ae49d91959a4e94152
sigmundholm <sigmundhol@hotmail.com>
2016-11-08 08:31:47 +0100

    add url for add_item and tag_detail

--- inventory/urls.py
+++ inventory/urls.py
@@ -7,8 +7,12 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<item_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^add_item/', views.add_item, name='add_item'),  # TODO
+    url(r'^(?P<item_id>[0-9]+)/add_item/$', views.add_item, name='add_item'),
+
     url(r'^add_tag/', views.add_tag, name='add_tag'),  # TODO
+    url(r'^(?P<tag_id>[0-9]+)/add_tag/$', views.add_tag, name='add_tag'),
+    url(r'^(?P<tag_id>[0-9]+)/tag_detail/$', views.tag_detail, name='tag_detail'),
+
     url(r'^loan/', views.loan, name='loan'),
     url(r'^registered/', views.registered, name='registered'),
-    url(r'^(?P<item_id>[0-9]+)/change_item/$', views.change_item, name='change_item'),
 ]

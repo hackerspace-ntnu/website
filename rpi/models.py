ca66703d90ac03cfe6a584bbcf9173c24b1244cf
Mathias Ose <mathias.ose@gmail.com>
2017-01-23 18:15:58 +0100

    Refactor and format RaspberryPi model

--- rpi/models.py
+++ rpi/models.py
@@ -1,19 +1,38 @@
 from os.path import join, dirname, realpath
 
-from django.core.validators import RegexValidator, validate_ipv46_address
+from django.core.validators import RegexValidator
 from django.db import models
 
 MAC_VALIDATOR = RegexValidator(r'^([a-f0-9]{2}:){5}[a-f0-9]{2}$')
-IP_VALIDATOR = validate_ipv46_address
 PI_NAMES_FILE = join(dirname(realpath(__file__)), 'availnames.txt')
 
 
 class RaspberryPi(models.Model):
-    name = models.CharField(max_length=50, unique=True, blank=False)
-    last_seen = models.DateTimeField('last seen', blank=False)
-    ip = models.CharField('IP', max_length=15, validators=[IP_VALIDATOR], blank=False)
-    mac = models.CharField('MAC', max_length=17, default='12:34:56:78:90:ab', validators=[MAC_VALIDATOR], unique=True,
-                           db_index=True, blank=False)
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        blank=False,
+        null=False,
+    )
+    last_seen = models.DateTimeField(
+        verbose_name='last seen',
+        blank=False,
+        null=False,
+    )
+    ip = models.GenericIPAddressField(
+        verbose_name='IP',
+        blank=False,
+        null=False,
+    )
+    mac = models.CharField(
+        verbose_name='MAC',
+        max_length=17,
+        validators=[MAC_VALIDATOR],
+        unique=True,
+        db_index=True,
+        blank=False,
+        null=False,
+    )
 
     def __str__(self):
         return self.name
@@ -26,7 +45,7 @@ class RaspberryPi(models.Model):
     @staticmethod
     def suggest_name():
         names = RaspberryPi.get_names()
-        used_names = RaspberryPi.object.values_list('name', flat=True)
+        used_names = RaspberryPi.objects.values_list('name', flat=True)
         available_names = set(names) - set(used_names)
         n = 0
         while not available_names:

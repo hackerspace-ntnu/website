1af5a1aa8f494c9b4a9e7489c36896a1ff3045a8
martsime <simensen94@gmail.com>
2017-02-21 19:49:22 +0100

    Fix fabfile to work with both production and dev environment

--- fabfile.py
+++ fabfile.py
@@ -1,21 +1,22 @@
 from fabric.api import env, run, cd
 
-env.hosts = ['potet.hackerspace-ntnu.no']
-root_folder = '/srv/docker-services/'
+env.hosts = ['hackerspace-ntnu.no']
+root_folder = '/devops/docker-services/'
 projects = {
         'potet': {
             'repo_folder': 'website'
         },
+        'prod': {
+            'repo_folder': 'website'
+        }
 }
-
 
 def pull():
     with cd(root_folder):
         run('git pull')
 
-
 def deploy(project, ref='master'):
-    with cd(root_folder + project + '/' + projects[project]['repo_folder']):
+    with cd(root_folder + project + '/' + projects[project]['repo_folder'] + '/'):
         run('git fetch --all')
         run('git checkout {0}'.format(ref))
         run('git reset --hard origin/{0}'.format(ref))
@@ -27,7 +28,7 @@ def deploy(project, ref='master'):
 
 
 def deploycommit(project, commit):
-    with cd(root_folder + project + '/' + projects[project]['repo_folder']):
+    with cd(root_folder + project + '/' + projects[project]['repo_folder'] + '/'):
         run('git fetch --all')
         run('git checkout {0}'.format(commit))
     with cd(root_folder+project):

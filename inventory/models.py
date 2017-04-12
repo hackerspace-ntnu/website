844ddf2a5ec2a9acb7da6a290d3b19d4d7d46725
Sigmund Eggen Holm <sigmund.holm@gmail.com>
2017-04-13 00:58:52 +0200

    Create field for image in Item.

--- inventory/models.py
+++ inventory/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.admin import User
 from django.db import models
 from django.utils import timezone
+from sorl.thumbnail import ImageField
 
 from datetime import timedelta
 
@@ -20,6 +21,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
     quantity = models.IntegerField(default=1)
+    image = ImageField(upload_to='inventory', null=True)
     visible = models.BooleanField(default=True)
 
     tags = models.ManyToManyField(Tag)
@@ -30,7 +32,6 @@ class Item(models.Model):
     place = models.IntegerField(null=True)
 
     # TODO legge til et felt for å telle popularitet i sidevisninger, kan bruke dette når man søker
-    # TODO antall lånt ut (må holde styr på antall man har tilgjengelig)?? finner via Loans
 
     def show_tags(self):
         all_tags = ", ".join(str(tag) for tag in self.tags.all())
@@ -41,8 +42,6 @@ class Item(models.Model):
 
 
 class Loan(models.Model):
-    MAX_ITEMS = 6  # maks antall items som kan legges til via form, hold tallet delelig på 2    for at det skal bli smoothere å dele inn formsida
-
     borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='loan_set')  # lånetaker
     lender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='lender_set')  # utlåner
 

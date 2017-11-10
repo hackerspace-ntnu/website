f5ee8a19e786a44f312391c5c393dc44713b8ee7
Sigmund Eggen Holm <sigmund.holm@gmail.com>
2017-11-10 15:40:56 +0100

    Get user based on email, not username (feide login)

--- authentication_feide/views.py
+++ authentication_feide/views.py
@@ -27,9 +27,9 @@ def index(request):
 
 def login(request):
     dataporten_auth_url = dataporten_oauth_client.prepare_request_uri(
-            settings.DATAPORTEN_OAUTH_AUTH_URL, 
-            redirect_uri=get_callback_redirect_url(request))
-    request.session['feided']=True
+        settings.DATAPORTEN_OAUTH_AUTH_URL,
+        redirect_uri=get_callback_redirect_url(request))
+    request.session['feided'] = True
 
     return HttpResponseRedirect(dataporten_auth_url)
 
@@ -38,17 +38,17 @@ def login_callback(request):
     code = request.GET.get('code')
 
     token_request_body = dataporten_oauth_client.prepare_request_body(
-            code=code,
-            redirect_uri=get_callback_redirect_url(request),
-            client_secret=settings.DATAPORTEN_OAUTH_CLIENT_SECRET)
+        code=code,
+        redirect_uri=get_callback_redirect_url(request),
+        client_secret=settings.DATAPORTEN_OAUTH_CLIENT_SECRET)
 
     token_request_response = requests.post(
-            settings.DATAPORTEN_OAUTH_TOKEN_URL,
-            data=token_request_body,
-            headers={ 
-                "content-type": "application/x-www-form-urlencoded",
-                "authorization": "Basic {}".format(settings.DATAPORTEN_OAUTH_CLIENT_SECRET),
-            })
+        settings.DATAPORTEN_OAUTH_TOKEN_URL,
+        data=token_request_body,
+        headers={
+            "content-type": "application/x-www-form-urlencoded",
+            "authorization": "Basic {}".format(settings.DATAPORTEN_OAUTH_CLIENT_SECRET),
+        })
 
     if token_request_response.status_code != 200:
         raise Exception("invalid code")
@@ -63,7 +63,11 @@ def login_callback(request):
     first_name = " ".join(user_info['user']['name'].split(" ")[0:-1])
     last_name = user_info['user']['name'].split(" ")[-1]
 
-    user = User.objects.get_or_create(username=username,email=user_email,first_name=first_name,last_name=last_name)
-    user[0].backend = 'django.contrib.auth.backends.ModelBackend'
-    auth_login(request,user[0])
+    try:
+        user = User.objects.get(email=user_email)
+    except User.DoesNotExist:
+        user = User.objects.create_user(username=username, email=user_email, first_name=first_name, last_name=last_name)
+
+    user.backend = 'django.contrib.auth.backends.ModelBackend'
+    auth_login(request, user)
     return HttpResponseRedirect('/')

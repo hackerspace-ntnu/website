5056aeb039b1f15103e9c02653c4a21df75910f3
sm0xe <sm0xe@protonmail.com>
2017-11-13 22:04:59 +0100

    Added try-catch for missing profiles

--- userprofile/templatetags/userprofile.py
+++ userprofile/templatetags/userprofile.py
@@ -4,5 +4,8 @@ from django import template
 register = template.Library()
 @register.simple_tag
 def get_profile(user_id,attr):
-    obj = getattr(Profile.objects.get(user__id=user_id), attr)
-    return obj
+    try:
+        return getattr(Profile.objects.get(user__id=user_id), attr)
+    except:
+        return ''
+

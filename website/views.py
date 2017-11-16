8d77303c3b0c20ddde94dd0b31d4ff0219c4f836
camelgod <god@e-grotto.faith>
2017-11-16 16:23:47 +0100

    Display some random images on front page

--- website/views.py
+++ website/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 from news.models import Article, Event
+from files.models import Image
 from door.models import DoorStatus
 from datetime import datetime
 from wiki.templatetags import check_user_group as groups
@@ -14,8 +15,11 @@ def index(request):
     # First sort, then grab 5 elements, then flip the list ordered by date
     event_list = Event.objects.filter(internal__lte=can_access_internal).order_by('-time_start')[:5:-1]
 
-    # Get three articles
+    # Get five articles
     article_list = Article.objects.filter(internal__lte=can_access_internal).order_by('-pub_date')[:5]
+
+    # Get some images for slideshow
+    image_list = Image.objects.order_by('-time')[:7]
 
     event_list = list(event_list)
     artile_list = list(article_list)
@@ -27,6 +31,7 @@ def index(request):
 
     context = {
         'article_list': article_list,
+        'image_list': image_list,
         'event_list': event_list,
         'door_status': door_status,
     }

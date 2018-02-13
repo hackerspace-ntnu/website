ce842b085c6ab1cf67c644c05149882effe59e3e
TommyWithATiger <halvard.hummel@hotmail.com>
2018-02-13 20:43:53 +0100

    Duplicate application URLs import

--- website/urls.py
+++ website/urls.py
@@ -22,7 +22,6 @@ urlpatterns = [
     url(r'^news/', include('news.urls')),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')),
     url(r'^authentication/', include('authentication.urls')),
-    url(r'^opptak/', include('applications.urls')),
     url(r'^door/', include('door.urls')),
     url(r'^ckeditor_uploader/', include('ckeditor_uploader.urls')),
     url(r'^opptak/', include('applications.urls'), name='opptak'),

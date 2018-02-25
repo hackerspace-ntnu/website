eea05e11b277b32ed67e785eac3c25ba638fa56c
camelgod <god@e-grotto.faith>
2018-02-25 13:11:31 +0100

    Rework ckeditor and articles / events

---
+++ website/static/ckeditor/ckeditor/config.js
@@ -0,0 +1,26 @@
+/**
+ *  * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ *   * For licensing, see LICENSE.md or http://ckeditor.com/license
+ *    */
+
+CKEDITOR.editorConfig = function( config ) {
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	config.stylesSet = [];
+	config.height = '100%';
+	config.coreStyles_bold = { element: 'b', overrides: 'strong' };
+};
+
+
+CKEDITOR.on('instanceReady', function( ev ) {
+	ev.editor.dataProcessor.htmlFilter.addRules({
+		elements: {
+			p: function (e) { 
+					e.attributes.class = 'flow-text';
+			},
+			strong: function (e) { e.attributes.class = 'flow-text'; },
+			ul: function (e) { e.attributes.class = 'article-ul'; },
+			li: function (e) { e.attributes.class = 'flow-text'; }
+		},
+	});
+});

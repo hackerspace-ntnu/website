13971073b3f38f38c6d0ffd5cb8af8eb90aaea6b
camelgod <god@e-grotto.faith>
2018-03-23 11:13:38 +0100

    Fix authenticationform in modal

--- authentication/templatetags/loginmodal.py
+++ authentication/templatetags/loginmodal.py
@@ -1,8 +1,8 @@
-from authentication.forms import LoginForm
+from django.contrib.auth.forms import AuthenticationForm
 from django import template
 
 register = template.Library()
 
 @register.inclusion_tag('login_modal.html')
 def LoginModal():
-    return {'login_form': LoginForm() }
+    return {'login_form': AuthenticationForm() }

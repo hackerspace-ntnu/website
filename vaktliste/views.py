e9aed6d02482b0d3da48fb63794f9a8764abe1da
camelgod <god@e-grotto.faith>
2018-03-23 16:39:33 +0100

    New test for queries

--- vaktliste/views.py
+++ vaktliste/views.py
@@ -134,12 +134,12 @@ def index(request):
         for day in vakt_data:
             for time in vakt_data[day]:
                 for name in vakt_data[day][time]:
+                    name.split()
                     try:
-                        name.split()
                         user = User.objects.get(first_name__icontains=name[0])
-                        userList.append(user)
+                        userList.append(user.get_full_name())
                     except:
-                        userList.append(name + "(Default)")
+                        userList.append(name[0] + "(Default)")
 
 
     context = {

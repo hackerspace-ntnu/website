d8b85a7ee9ad78b642fa4d75e0590412b373f318
camelgod <god@e-grotto.faith>
2018-03-29 04:30:48 +0200

    Fix serializer for nested object, make list look pretty :sparkles:

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -2,14 +2,17 @@ from rest_framework import serializers
 from .models import Item, Tag
 
 
-class ItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Item
-        fields = ('id','name','description', 'quantity', 'thumbnail', 'tags', 'zone', 'shelf', 'row', 'column')
-        depth = 2
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ('name', 'parent_tag', 'item_set')
+        fields = ('id', 'name', 'parent_tag', 'item_set')
+        depth = 2
+
+class ItemSerializer(serializers.ModelSerializer):
+    tags = serializers.PrimaryKeyRelatedField(many=True, read_only=False, queryset = Tag.objects.all())
+
+    class Meta:
+        model = Item
+        fields = ('id','name','description', 'quantity', 'thumbnail', 'tags', 'zone', 'shelf', 'row', 'column')
         depth = 2

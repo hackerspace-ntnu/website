deb6c860ed3227917186c60fd7f79bec8f9f224f
camelgod <god@e-grotto.faith>
2018-03-29 15:41:15 +0200

    Restructure serialization, autoselect tag objects in dropdown, better materialize implementation, nested object structure improvement

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -2,17 +2,14 @@ from rest_framework import serializers
 from .models import Item, Tag
 
 
-
-class TagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tag
-        fields = ('id', 'name', 'parent_tag', 'item_set')
-        depth = 2
-
 class ItemSerializer(serializers.ModelSerializer):
-    tags = serializers.PrimaryKeyRelatedField(many=True, read_only=False, queryset = Tag.objects.all())
-
     class Meta:
         model = Item
         fields = ('id','name','description', 'quantity', 'thumbnail', 'tags', 'zone', 'shelf', 'row', 'column')
+
+class TagSerializer(serializers.ModelSerializer):
+    item_set = ItemSerializer(many=True)
+    class Meta:
+        model = Tag
+        fields = ('id', 'name', 'parent_tag', 'item_set')
         depth = 2

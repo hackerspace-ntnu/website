4604e36504f69739cc7292fbbeebf9465236790a
camelgod <god@e-grotto.faith>
2018-03-30 01:43:27 +0200

    Remove unused stuff

--- inventory/models.py
+++ inventory/models.py
@@ -8,9 +8,8 @@ from files.models import Image
 class Tag(models.Model):
     name = models.CharField(max_length=100, unique=True)
     visible = models.BooleanField(default=True)
-
-    # TODO lag restriction så den ikke kan være seg selv
-    parent_tag = models.ForeignKey('Tag', null=True, blank=True, related_name="children_tags")
+    parent_tag = models.ForeignKey('Tag', null=True, blank=True,
+                                   related_name="children_tags")
 
     def get_visible_items(self):
         return self.item_set.filter(visible=True)
@@ -23,7 +22,8 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=300)
     quantity = models.IntegerField(default=1)
-    thumbnail = models.ForeignKey(Image, on_delete=models.SET_NULL, blank=True, null=True)
+    thumbnail = models.ForeignKey(Image, on_delete=models.SET_NULL,
+                                  blank=True, null=True)
     visible = models.BooleanField(default=True)
     tags = models.ManyToManyField(Tag)
 
@@ -47,22 +47,23 @@ class Item(models.Model):
         return str("<Item>" + self.name)
 
 
-
 class Loan(models.Model):
     borrower = models.CharField(max_length=300, null=True)
     email = models.CharField(max_length=300, null=True)
     phone = models.CharField(max_length=300, null=True)
-    lender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='lender_set')  # utlåner
+    lender = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
+                               related_name='lender_set')
 
     comment = models.CharField(max_length=300)
     visible = models.BooleanField(default=True)
-    item = models.ForeignKey('Item', on_delete=models.CASCADE, null=True, related_name="loans")
-    quantity = models.IntegerField(default=1)  # Antall man har lånt av typen item.
+    item = models.ForeignKey('Item', on_delete=models.CASCADE, null=True,
+                             related_name="loans")
+    # Antall man har lånt av typen item.
+    quantity = models.IntegerField(default=1)
 
     loan_date = models.DateTimeField('date_lent', default=timezone.now)
     return_date = models.DateTimeField('return_date', default=timezone.now)
-    date_returned = models.DateTimeField('date_returned', null=True)  # innleveringstidspunkt
+    date_returned = models.DateTimeField('date_returned', null=True)
 
     def is_past_return_date(self):
         return self.return_date - timezone.now() < timedelta()
-
--- userprofile/urls.py
+++ userprofile/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'userprofile'
 
 urlpatterns = [
-    url(r'^members/$', views.members),
+    url(r'members/$', views.members, name='members'),
     url(r'^$', views.profile, name='profile'),
     url(r'^(?P<user_id>[0-9]+)$', views.specific_profile, name='profile_by_id'),
     url(r'^edit', views.edit_profile, name="edit_profile"),

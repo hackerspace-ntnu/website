5733eb4e998a7ead6c4b4e891d8c86e18aa83c98
camelgod <god@e-grotto.faith>
2018-03-30 03:19:10 +0200

    Prepare test setup for new rest framework

---
+++ inventory/tests.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.test import TestCase
+
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.tag_data = {"id": 2, "name": "Test Tag", "item_set": []}
+
+    def test_authorization_is_enforced(self):
+        new_client = APIClient()
+        res = new_client.get('/inventory/tags/1', format="json")
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
--- inventory/urls.py
+++ inventory/urls.py
@@ -5,9 +5,9 @@ app_name = 'inventory'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^tags/$', views.TagList.as_view()),
-    url(r'^tags/(?P<pk>[0-9]+)$', views.TagDetail.as_view(), name='view-specific-tag'),
-    url(r'^items/(?P<pk>[0-9]+)$', views.ItemDetail.as_view(), name='view-specific-item'),
-    url(r'^items/$', views.ItemList.as_view(), name='view-list-item'),
-    url(r'^loans/$', views.LoanList.as_view(), name='view-list-loan'),
+    url(r'^tags/(?P<pk>[0-9]+)$', views.TagDetail.as_view(), name='specific-tag'),
+    url(r'^items/(?P<pk>[0-9]+)$', views.ItemDetail.as_view(), name='specific-item'),
+    url(r'^items/$', views.ItemList.as_view(), name='list-item'),
+    url(r'^loans/$', views.LoanList.as_view(), name='list-loan'),
+    url(r'^tags/$', views.TagList.as_view(), name='list-tag'),
 ]

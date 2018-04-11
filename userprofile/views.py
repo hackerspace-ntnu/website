b72171c93e23d6b03916beb1666b00254a1de1e2
camelgod <god@e-grotto.faith>
2018-04-11 15:23:36 +0200

    Update references to userprofile by ID, new styling on skills / member list

--- userprofile/views.py
+++ userprofile/views.py
@@ -40,6 +40,7 @@ def members(request):
 def skill(request, skill_title):
     skill = Skill.objects.get(title=skill_title)
     profiles = Profile.objects.filter(skills__title__icontains=skill_title, group__isnull=False).distinct()
+
     context = {'skill': skill, 'profiles': profiles}
     return render(request, 'userprofile/skill.html', context)
 

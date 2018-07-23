605b5860fbf18a72b946d5579462ca3db8168782
camelgod <god@e-grotto.faith>
2018-07-23 10:32:07 +0200

    Redo file uploading system

--- files/urls.py
+++ files/urls.py
@@ -4,8 +4,7 @@ from . import views
 urlpatterns = [
     #url(r'^image/(?P<title>[0-9]+)/$', views.image, name='image'),
     url(r'^images/$', views.images, name='images'),
-    url(r'^image-upload/$', views.imageUpload, name='image-upload'),
-    url(r'^image/(?P<image_id>[0-9]+)/delete', views.imageDelete, name='image-delete'),
-    url(r'^image/(?P<image_id>[0-9]+)/edit', views.imageEdit, name='image-edit'),
-    url(r'^image/(?P<image_id>[0-9]+)/view', views.imageView, name='image-view'),
+    url(r'^image-upload/', views.imageUpload, name='image-upload'),
+    url(r'^image-newest/', views.ajax_return_last_image, name='recent-upload'),
+    url(r'^image/(?P<image_id>[0-9]+)/view', views.imageView, name='image'),
 ]

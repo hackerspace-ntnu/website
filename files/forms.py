2d84b2ee8dd4bfcd7129a3c7e2c4571e8ec04bf9
Camelgod <god@e-grotto.faith>
2018-07-24 15:43:57 +0200

    Style tweaks

--- files/forms.py
+++ files/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
+from django.forms import TextInput
 from .models import Image
 
 class ImageForm(ModelForm):
     class Meta:
         model = Image
         fields = ['description', 'title', 'file']
+        widgets = {
+            'description': TextInput()
+        }
+
+

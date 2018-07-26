1ecb5ec198d4810ca15736c2de7922b4cb91e03e
Camelgod <god@e-grotto.faith>
2018-07-26 11:32:50 +0200

    Class based views for events

--- news/urls.py
+++ news/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'news'
 urlpatterns = [
-    url(r'^$', views.ArticleListView.as_view(), name='all-news'),
-    url(r'^(?P<pk>[0-9]+)/$', views.ArticleView.as_view(), name='article'),
-    url(r'^(?P<article_id>[0-9]+)/edit', views.edit_article, name='edit-article'),
-    url(r'^(?P<article_id>[0-9]+)/delete', views.delete_article, name='delete-article'),
+    url(r'^$', views.ArticleListView.as_view(), name='all'),
+    url(r'^(?P<pk>[0-9]+)/$', views.ArticleView.as_view(), name='details'),
+    url(r'^(?P<pk>[0-9]+)/edit', views.ArticleUpdateView.as_view(), name='edit'),
+    url(r'^new', views.ArticleCreateView.as_view(), name='new'),
+    url(r'^(?P<pk>[0-9]+)/delete', views.ArticleDeleteView.as_view(), name='delete'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,29 +1,21 @@
-from django.http import HttpResponseRedirect, Http404, HttpResponse, JsonResponse
-from django.contrib.auth.mixins import PermissionRequiredMixin
-from django.shortcuts import get_object_or_404, render
-from django.utils import formats
+from django.shortcuts import get_object_or_404, redirect
 from django.utils import timezone
-from django.views.generic import TemplateView, DetailView, ListView
-from datetime import datetime, timedelta
-from . import log_changes
-from .forms import ArticleEditForm, AttendeeForm, EventForm
+from django.views.generic import TemplateView, DetailView, ListView, FormView, UpdateView, CreateView, DeleteView
+from datetime import datetime
+from .forms import EventForm
 from .models import Event, Article, EventRegistration
-from itertools import chain
 from authentication.templatetags import check_user_group as groups
-from django.contrib.auth.admin import User
 from django.contrib.auth.decorators import login_required
-from files.models import Image
-
 
 class EventView(DetailView):
     model = Event
     template_name = "news/event.html"
 
-    def get_queryset(self):
-        if self.event.internal and not groups.has_group(self.request.user, 'member'):
-            return Event.objects.get(pk)
-        else:
-            return HttpResponseRedirect("/")
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().internal and not groups.has_group(self.request.user, 'member'):
+            return redirect("/")
+
+        return super(EventView, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
@@ -66,149 +58,112 @@ class ArticleListView(ListView):
 
 
 class ArticleView(DetailView):
+    model = Article
     template_name = "news/article.html"
 
-    def get_queryset(self):
-        if groups.has_group(self.request.user, 'member'):
-            return Article.objects.get(pk)
-        else:
-            return HttpResponseRedirect("/")
+    def dispatch(self, request, *args, **kwargs):
+        # Check if user is member if the article field internal is True
+        if self.get_object().internal and not groups.has_group(request.user, 'member'):
+            return redirect("/")
+
+        return super(ArticleView, self).dispatch(request, *args, **kwargs)
 
 
-def article(request, article_id):
-    article_id = get_id_or_404(article_id)
-    article = get_object_or_404(Article, pk=article_id)
-    context = {
-        'article': article,
-    }
+class EventUpdateView(UpdateView):
+    model = Event
+    template_name = "news/edit_event.html"
+    form_class = EventForm
+    success_url = "/events/"
 
-    if article.internal and not groups.has_group(request.user, 'member'):
-        return HttpResponseRedirect('/')
+    def dispatch(self, request, *args, **kwargs):
+        if not groups.has_group(self.request.user, 'member'):
+            return redirect("/")
 
-    return render(request, 'news/article.html', context)
+        return super(EventUpdateView, self).dispatch(request, *args, **kwargs)
+
+    def get_initial(self):
+        initial = super(EventUpdateView, self).get_initial()
+        initial['time_start'] = self.object.time_start.date().strftime('%Y-%m-%d')
+        initial['time_end'] = self.object.time_end.date().strftime('%Y-%m-%d')
+        initial['event_start_time'] = self.object.time_start.time()
+        initial['event_end_time'] = self.object.time_end.time()
+        initial['registration_start_time'] = self.object.registration_start.time()
+        initial['registration_start'] = self.object.registration_start.date().strftime('%Y-%m-%d')
+        initial['deregistration_end'] = self.object.deregistration_end.date().strftime('%Y-%m-%d')
+        initial['deregistration_end_time'] = self.object.deregistration_end.time()
+        return initial
 
 
-def NewEvent(request):
-    if request.method == 'POST':  # Post form
-        form = EventForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/events/')
+class EventCreateView(CreateView):
+    model = Event
+    template_name = "news/edit_event.html"
+    form_class = EventForm
+    success_url = "/events/"
 
-    else:
-        # No event to edit, set data to default
-        # Set initial values
+    def dispatch(self, request, *args, **kwargs):
+        if not groups.has_group(self.request.user, 'member'):
+            return redirect("/")
+
+        return super(EventCreateView, self).dispatch(request, *args, **kwargs)
+
+    def get_initial(self):
         today = datetime.strftime(timezone.now(), '%Y-%m-%d')
-        form = EventForm(initial={
-            'event_start_time': '00:00',
-            'time_start': today,
-            'event_end_time': '00:00',
-            'time_end': today,
-            'registration_start_time': '00:00',
-            'registration_start': today,
-            'deregistration_end': today,
-            'deregistration_end_time': '00:00',
-        })
 
-    context = {
-        'form': form,
-    }
+        initial = super(EventCreateView, self).get_initial()
+        initial['event_start_time'] = "00:00"
+        initial['event_end_time'] = "00:00"
+        initial['registration_start_time'] = "00:00"
+        initial['deregistration_end_time'] = "00:00"
 
-    return render(request, 'news/edit_event.html', context)
-
-def edit_event(request, event_id):
-    instance = get_object_or_404(Event, pk=event_id)
-    if request.method == 'POST':  # Post form
-        form = EventForm(request.POST, instance=instance)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/events/' + event_id + '/')
-    else:
-        # Set values for edit-form
-        form = EventForm(instance=instance, initial = {
-            'time_start': datetime.strftime(instance.time_start.date(), '%Y-%m-%d'),
-            'time_end': datetime.strftime(instance.time_end.date(), '%Y-%m-%d'),
-            'event_start_time': instance.time_start.time(),
-            'event_end_time': instance.time_end.time(),
-            'registration_start_time': instance.registration_start.time(),
-            'registration_start': datetime.strftime(instance.registration_start.date(), '%Y-%m-%d'),
-            'deregistration_end': datetime.strftime(instance.deregistration_end.date(), '%Y-%m-%d'),
-            'deregistration_end_time': instance.deregistration_end.time()
-        })
-
-    context = {
-        'form': form,
-    }
-
-    return render(request, 'news/edit_event.html', context)
+        initial['time_start'] = today
+        initial['time_end'] = today
+        initial['registration_start'] = today
+        initial['deregistration_end'] = today
+        return initial
 
 
-def edit_article(request, article_id):
-    article_id = get_id_or_404(article_id)
-    if request.method == 'POST':  # Post form
-        form = ArticleEditForm(request.POST)
-        if form.is_valid():
-            if article_id == 0:
-                article = Article()
-            else:
-                article = get_object_or_404(Article, pk=article_id)
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = ['title', 'ingress_content', 'main_content', 'thumbnail', 'internal']
+    template_name = "news/edit_article.html"
+    success_url = "/news/"
 
-            article.title = form.cleaned_data['title']
-            article.ingress_content = form.cleaned_data['ingress_content']
-            article.main_content = form.cleaned_data['main_content']
-            thumbnail_raw = form.cleaned_data['thumbnail']
-            article.internal = form.cleaned_data['internal']
-            try:
-                thumb_id = int(thumbnail_raw)
-                article.thumbnail = Image.objects.get(id=thumb_id)
-            except (TypeError, ValueError, Image.DoesNotExist):
-                article.thumbnail = None
-            article.save()
-            log_changes.change(request, article)
+    def dispatch(self, request, *args, **kwargs):
+        if not groups.has_group(self.request.user, 'member'):
+            return redirect("/")
+        return super(ArticleCreateView, self).dispatch(request, *args, **kwargs)
 
-            return HttpResponseRedirect('/news/' + str(article.id) + '/')
-    else:  # Request form
-        if article_id == 0:
-            form = ArticleEditForm()
-        else:
-            article = get_object_or_404(Article, pk=article_id)
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = "news/edit_article.html"
+    fields = ['title', 'ingress_content', 'main_content', 'thumbnail', 'internal']
+    success_url = "/news/"
 
-            try:
-                thumb_id = article.thumbnail.id
-            except AttributeError:
-                thumb_id = 0
+    def dispatch(self, request, *args, **kwargs):
+        if not groups.has_group(self.request.user, 'member'):
+            return redirect("/")
 
-            # Set values for edit-form
-            form = ArticleEditForm(initial={
-                'title': article.title,
-                'ingress_content': article.ingress_content,
-                'main_content': article.main_content,
-                'thumbnail': thumb_id,
-                'internal': article.internal,
-            })
-    context = {
-        'form': form,
-    }
+        return super(ArticleUpdateView, self).dispatch(request, *args, **kwargs)
 
-    return render(request, 'news/edit_article.html', context)
+class ArticleDeleteView(DeleteView):
+    model = Article
+    success_url = "/news/"
 
+    def dispatch(self, request, *args, **kwargs):
+        if not groups.has_group(self.request.user, 'member'):
+            return redirect("/")
 
-def delete_article(request, article_id):
-    article_id = get_id_or_404(article_id)
-    if groups.has_group(request.user, 'member'):
-        article = get_object_or_404(Article, pk=article_id)
-        article.delete()
+        return super(ArticleDeleteView, self).dispatch(request, *args, **kwargs)
 
-    return HttpResponseRedirect('/news/')
+class EventDeleteView(DeleteView):
+    model = Event
+    success_url = "/events/"
 
+    def dispatch(self, request, *args, **kwargs):
+        if not groups.has_group(self.request.user, 'member'):
+            return redirect("/")
 
-def delete_event(request, event_id):
-    event_id = get_id_or_404(event_id)
-    if groups.has_group(request.user, 'member'):
-        event = get_object_or_404(Event, pk=event_id)
-        event.delete()
-
-    return HttpResponseRedirect('/events/')
+        return super(EventDeleteView, self).dispatch(request, *args, **kwargs)
 
 
 @login_required
@@ -223,49 +178,5 @@ def register_on_event(request, event_id):
         if now > event_object.registration_start and event_object.time_end > now:
             EventRegistration.objects.create(event=event_object, user=request.user).save()
 
-    return HttpResponseRedirect("/events/" + event_id)
+    return redirect("/events/" + event_id)
 
-
-def event_attendees(request, event_id):
-    if request.method == 'POST':
-        try:
-            user_string = request.POST['name']
-            username = user_string.split("-")[-1].strip()
-            user = User.objects.get(username=username)
-            event = Event.objects.get(pk=event_id)
-            er = EventRegistration.objects.get(event=event, user=user)
-            name = er.name() if er.name().strip() != '' else username
-
-            if not er.attended:
-                er.attended = True
-                er.save()
-                message = name + ' er nå registrert'
-            else:
-                message = name + ' er allerede registrert'
-
-            return JsonResponse({'success': True, 'message': message, 'username': username}, safe=False)
-
-        except IndexError:
-            return JsonResponse({'success': False, 'message': 'Fant ikke bruker'}, safe=False)
-    else:
-        event_object = get_object_or_404(Event, pk=get_id_or_404(event_id))
-
-        form = AttendeeForm()
-
-        context = {
-            'id': event_id,
-            'form': form,
-            'event': event_object,
-            'users': EventRegistration.objects.filter(event=event_object),
-            'attending_usernames': event_object.attending_usernames(),
-        }
-
-        return render(request, 'news/event_attendees.html', context)
-
-
-def get_id_or_404(object_id):
-    object_id = int(object_id)
-    # Raise 404 if ID too large for SQLite (2^63-1) or negative
-    if object_id > 9223372036854775807 or object_id < 0:
-        raise Http404
-    return object_id

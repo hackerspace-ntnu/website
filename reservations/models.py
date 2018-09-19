250fa5672e6b9e4e6da5dad2163255fe3ef948f3
Sebastian Ellefsen <sebastae@stud.ntnu.no>
2018-09-19 19:32:02 +0200

    Defined Queue and Reservation models

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import CharField, TextField, BooleanField, TimeField, ForeignKey, DateField
 
-# Create your models here.
+
+class Queue(models.Model):
+    name = CharField(max_length=64, blank=False, unique=True)
+    description = TextField(max_length=512, default="")
+
+    # Visible for regular users
+    published = BooleanField(default=False)
+
+    start_time = TimeField(blank=False)
+    end_time = TimeField(blank=False)
+
+    def __str__(self):
+        return self.name + "[%s]" % self.reservations.count()
+
+
+class Reservation(models.Model):
+    parent_queue = ForeignKey(Queue, related_name='reservations', on_delete=models.CASCADE)
+    user = ForeignKey(User, related_name='reservations', on_delete=models.CASCADE)
+
+    date = DateField(blank=False)
+    start_time = TimeField(blank=False)
+    end_time = TimeField(blank=False)
+
+    def __str__(self):
+        return self.user.username + "[%s.%s.%s]" % self.date.isocalendar()

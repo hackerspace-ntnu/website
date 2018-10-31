8cd94c8795d3933d365173c98580d543103baeb3
Sebastian André Ellefsen <sebastae@stud.ntnu.no>
2018-10-31 21:05:43 +0100

    Better selection of date and time

--- reservations/forms.py
+++ reservations/forms.py
@@ -1,6 +1,8 @@
 import datetime
-from django.forms import ModelForm, ValidationError, widgets
+
+from django.forms import ModelForm, ValidationError, TimeInput, DateInput
 from django.shortcuts import get_object_or_404
+
 from reservations.models import Queue, Reservation
 
 
@@ -16,15 +18,23 @@ class ReservationForm(ModelForm):
     class Meta:
         model = Reservation
         fields = ('date', 'start_time', 'end_time', )
+        widgets = {
+            'date': DateInput(attrs={'class': 'datepicker .no-autoinit'}),
+            'start_time': TimeInput(attrs={'class': 'timepicker .no-autoinit'}),
+            'end_time': TimeInput(attrs={'class': 'timepicker .no-autoinit'}),
+        }
 
     def __init__(self, *args, **kwargs):
         self.parent_queue = get_object_or_404(Queue, pk=kwargs.pop('pk'))
         super().__init__(*args, **kwargs)
 
     def clean(self):
-        start_time = self.cleaned_data.get('start_time', None)
-        end_time = self.cleaned_data.get('end_time', None)
-        date = self.cleaned_data.get('date', None)
+
+        cleaned_data = super(ReservationForm, self).clean()
+
+        start_time = cleaned_data.get('start_time', None)
+        end_time = cleaned_data.get('end_time', None)
+        date = cleaned_data.get('date', None)
 
         if start_time > end_time:
             raise ValidationError("Invalid argument parameters")
@@ -36,3 +46,5 @@ class ReservationForm(ModelForm):
             reservation_overlap = interval[0] <= start_time, end_time <= interval[1]
             if reservation_overlap.__contains__(True):
                 raise ValidationError("Reservation incompatible with existing reservations")
+
+        return cleaned_data

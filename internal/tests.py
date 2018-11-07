93fe42f9cb36315c8ca7863937d2349cf2d7d2bf
zootos <halvard.hummel@hotmail.com>
2018-11-07 19:24:32 +0100

    Tests

--- internal/tests.py
+++ internal/tests.py
@@ -1,7 +1,8 @@
 from datetime import time, datetime
+from django.contrib.auth.models import User, Permission
 from django.test import TestCase
-
 # Create your tests here.
+from django.urls import reverse
 from unittest import mock
 
 from internal.models import TimeTable, TimeTableSlotSignup, TimeTableSlot
@@ -37,3 +38,78 @@ class TimeTableTest(TestCase):
         mocked_datetime.now.return_value = datetime(2009, 6, 30)
         self.assertEqual("09V", TimeTable.current_term())
 
+
+class TimeTableSignupViewTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user("test_user", password="password")
+        TimeTable.create(4, "18H", time(10, 15), 3)
+        self.add_permission = Permission.objects.get(codename="change_timetableslotsignup")
+        self.client.login(username="test_user", password="password")
+
+    def get_slot(self):
+        return TimeTableSlotSignup.objects.first()
+
+    def get_signup_url(self):
+        return reverse("change_hours", kwargs={"pk": self.get_slot().pk})
+
+    def test_change_allowed(self):
+        self.user.user_permissions.add(self.add_permission)
+        response = self.client.post(self.get_signup_url(), data={"user": self.user.pk})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.get_slot().user, self.user)
+
+        response = self.client.post(self.get_signup_url(), data={"user": 0})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.get_slot().user, None)
+
+    def test_change_other_no_admin(self):
+        self.user.user_permissions.add(self.add_permission)
+        user_other = User.objects.create_user("test_other_user")
+        user_other.user_permissions.add(self.add_permission)
+        slot = self.get_slot()
+        slot.user = user_other
+        slot.save()
+        self.assertEqual(self.get_slot().user, user_other)
+
+        response = self.client.post(self.get_signup_url(), data={"user": self.user.pk})
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(self.get_slot().user, user_other)
+
+        response = self.client.post(self.get_signup_url(), data={"user": 0})
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(self.get_slot().user, user_other)
+
+    def test_change_without_permission(self):
+        response = self.client.post(self.get_signup_url(), data={"user": self.user.pk})
+        # Redirect to login page
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(self.get_slot().user, None)
+
+    def test_change_other_admin(self):
+        self.user.user_permissions.add(Permission.objects.get(codename="admin_office_hours"))
+        self.user.user_permissions.add(self.add_permission)
+        user_other = User.objects.create_user("test_other_user")
+        user_other.user_permissions.add(self.add_permission)
+
+        # Check that admins can change to other people
+        response = self.client.post(self.get_signup_url(), data={"user": user_other.pk})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.get_slot().user, user_other)
+
+        # Check that admins can change away from other people
+        response = self.client.post(self.get_signup_url(), data={"user": self.user.pk})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.get_slot().user, self.user)
+
+    def test_signup_two_slots(self):
+        self.user.user_permissions.add(self.add_permission)
+        user_other = User.objects.create_user("test_other_user")
+
+        slot = TimeTableSlotSignup.objects.all()[1]
+        slot.user = user_other
+        slot.save()
+
+        response = self.client.post(self.get_signup_url(), data={"user": self.user.pk})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.get_slot().user, self.user)
--- internal/urls.py
+++ internal/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from internal.views import TimeTableView, SignupView
 
 urlpatterns = [
-    path("hours", TimeTableView.as_view()),
-    path("hours/change/<int:pk>", SignupView.as_view()),
+    path("hours", TimeTableView.as_view(), name="hours"),
+    path("hours/change/<int:pk>", SignupView.as_view(), name="change_hours"),
 ]

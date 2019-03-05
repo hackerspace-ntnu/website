e9d6fb28aa62530d1667c59b343a19188eaa70d8
riphiphip <halvor.bjorstad@gmail.com>
2019-03-05 13:56:40 +0100

    Logooppdatering fungerer, fikset skrivefeil i logonavn

--- seasonal_events/models.py
+++ seasonal_events/models.py
@@ -3,18 +3,28 @@ from django.db import models
 import datetime
 # Create your models here.
 
-
+#Date conflicts can arise
 class Season(models.Model):
     name = models.CharField(max_length=50, verbose_name="navn")
+    header_name = models.CharField(default="", max_length=50, verbose_name="headernavn")
     start_date = models.DateTimeField(verbose_name="startdato")
     end_date = models.DateTimeField(verbose_name="sluttdato")
+    active = models.BooleanField(default=True, verbose_name="aktiv")
+    repeating = models.BooleanField(default=False, verbose_name="repeterende")
     manual_override = models.BooleanField()
 
     def isNow(self):
-        if self.start_date <= datetime.now() <= self.end_date:
-            return True
-        else:
+        if not self.active:
             return False
+        if self.manual_override:
+            return True
+        if not self.repeating:
+            if self.start_date <= datetime.datetime.now() <= self.end_date:
+                return True
+        else:
+            if (self.start_date.day <= datetime.datetime.now().day <= self.end_date.day) and (self.start_date.month <= datetime.datetime.now().month <= self.end_date.month):
+                return True
+        return False
 
     def __str__(self):
         return self.name
--- seasonal_events/templatetags/seasonal_header_updater.py
+++ seasonal_events/templatetags/seasonal_header_updater.py
@@ -1,9 +1,12 @@
 from django import template
-
+from seasonal_events.models import Season
 register = template.Library()
 
 
-@register.inclusion_tag('website/header.html')
-def seasonal_header_updater():
-    currentSeason = "none"
-    return {"currentSeason": currentSeason}
+#Date conflicts can arise
+@register.simple_tag()
+def current_season():
+    for s in Season.objects.filter(active=True):
+        if s.isNow():
+            return "seasonal_events/seasonal_logos/"+s.header_name+".png"
+    return "website/img/logo/Hackerspace_huge.png"

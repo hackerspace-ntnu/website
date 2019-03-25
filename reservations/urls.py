8cbd4199dfd62c653a20e01eaa215b2fcf260ed5
Jonatan S. Engstad <jonaengs@gmail.com>
2019-03-25 18:21:17 +0100

    cleanup

--- reservations/urls.py
+++ reservations/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from django.views.generic import ListView, CreateView
 
+from reservations.models import Queue
 from reservations.views import QueueDetail, ReservationViewSet
 
 app_name = "reservations"
 
 urlpatterns = [
+        path('', ListView.as_view(model=Queue), name='queue_list'),
+        path('create/', CreateView.as_view(model=Queue, fields='__all__'), name='create_queue'),
         path('queue/<int:pk>/', QueueDetail.as_view(), name='queue_detail'),
         path('testing/<int:pk>/', ReservationViewSet.as_view({'get': 'list'}), name='fullcalendar_test'),
 

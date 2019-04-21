d153946aefbafd80aef731edaf039cb58f73c2be
Jonatan S. Engstad <jonaengs@gmail.com>
2019-04-21 22:59:35 +0200

    remove outdated tests

--- reservations/tests.py
+++ reservations/tests.py
@@ -1,9 +1,7 @@
-from django.contrib.auth.models import User, AnonymousUser, Permission
-from django.test import TestCase, RequestFactory, Client
-from django.urls import reverse
+from django.contrib.auth.models import User, Permission
+from django.test import TestCase, Client
 
 from reservations.models import Reservation, Queue
-from reservations.views import QueueDetailView
 
 
 class ReservationsTestCase(TestCase):
@@ -14,43 +12,9 @@ class ReservationsTestCase(TestCase):
         user.user_permissions.add(permission)
 
     def setUp(self):
-        self.queue_shown = Queue.objects.create(name="TestQueueOpen",
-                                                description="Test Description",
-                                                published=True)
-        self.queue_hidden = Queue.objects.create(name="TestQueueHidden",
-                                                 description="Another test description",
-                                                 published=False)
-        self.user = User.objects.create_user(username="adam", email="adam@eve.com", password="test_pass")
-        self.user2 = User.objects.create_user(username="ada", email="", password="bleh")
+        self.queue = Queue.objects.create(name="TestQueueOpen", description="Test Description", hidden=False)
+        self.queue_hidden = Queue.objects.create(name="TestQueueHidden", description="Another test description", hidden=True)
+        self.user = User.objects.create_user(username="USER", password="PASSWORD")
+        self.user2 = User.objects.create_user(username="USER2", password="PASSWORD")
         self.add_permission("add_queue", self.user)
-
-    def test_get_open_queue_anon(self):
-        response = self.client.get("/reservations/queue/1")
-        self.assertEqual(response.status_code, 200)
-
-    def test_get_open_queue_user(self):
-        self.client.login(username="adam", password="test_pass")
-        response = self.client.get("/reservations/queue/1")
-        self.assertEqual(response.status_code, 200)
-
-    def test_get_hidden_queue_anon(self):
-        self.client.logout()
-        url = "/reservations/queue/2"
-        response = self.client.get(url)
-        self.assertRedirects(response, reverse("auth:login") + "?next=" + url)
-
-    def test_get_hidden_queue_wrong_permission(self):
-        self.client.login(username="ada", password="bleh")
-        url = "/reservations/queue/2"
-        response = self.client.get(url)
-        self.assertRedirects(response, reverse("auth:login") + "?next=" + url)
-
-    def test_get_hidden_queue_user(self):
-        self.client.login(username="adam", password="test_pass")
-        response = self.client.get("/reservations/queue/2")
-        self.assertEqual(response.status_code, 200)
-
-    def test_get_nonexisting(self):
-        self.client.logout()
-        response = self.client.get('/reservations/queue/300')
-        self.assertEqual(response.status_code, 404)
+        self.client = Client.login(username=self.user.username, password=self.user.password)

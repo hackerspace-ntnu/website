6515cccb23d0d46923fc3c7d0dd8f15ea3127452
camelgod <god@e-grotto.faith>
2019-05-23 12:53:58 +0200

    Filter expired events, style fullcalendar, fix comment bug, add mobile check for dayview on mobile devices

--- reservations/urls.py
+++ reservations/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from django.views.generic import ListView
 
 from reservations.models import Queue
-from reservations.views import QueueDetailView
+from reservations.views import QueueDetailView, QueueListView
 
 
 app_name = "reservations"
 
 
 urlpatterns = [
-        path('', ListView.as_view(model=Queue), name='queue_list'),
+        path('', QueueListView.as_view(), name='queue_list'),
         path('queue/<int:pk>/', QueueDetailView.as_view(), name='queue_detail')
 ]

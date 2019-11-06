0e8ef12c612b278648e1e84a92bb5e3c6da5130e
Camelgod <god@e-grotto.faith>
2019-11-06 18:09:37 +0100

    remove files

--- internal/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from internal.models import TimeTable, TimeTableSlotSignup
-
-admin.site.register(TimeTable)
-admin.site.register(TimeTableSlotSignup)

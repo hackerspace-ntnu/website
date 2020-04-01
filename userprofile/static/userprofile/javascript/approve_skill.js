c24ebe56ea02fc5fefe3693645b6741cb217e3fe
riphiphip <halvor.bjorstad@gmail.com>
2020-04-01 20:19:22 +0200

    Added approval feature to level system

---
+++ userprofile/static/userprofile/javascript/approve_skill.js
@@ -0,0 +1,48 @@
+$(function () {
+    $.ajaxSetup({
+        beforeSend: function (xhr, settings) {
+            function getCookie(name) {
+                var cookieValue = null;
+                if (document.cookie && document.cookie != '') {
+                    var cookies = document.cookie.split(';');
+                    for (var i = 0; i < cookies.length; i++) {
+                        var cookie = jQuery.trim(cookies[i]);
+                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                            break;
+                        }
+                    }
+                }
+                return cookieValue;
+            }
+            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        }
+    });
+
+    $(".approve-btn").click(function () {
+        skillID = parseInt($(this).data('skillid'));
+        button = $(this)
+        payload = JSON.stringify(
+            {
+                skill_id: skillID
+            }
+        )
+        $.ajax({
+            url: window.location.href + "approve/",
+            method: 'POST',
+            data: payload,
+            dataType: 'json',
+            contentType: 'application/json; charset=UTF-8',
+            success: function (data) {
+                button.addClass('disabled')
+                button.text('Godkjent')
+                button.parentsUntil("li").filter("div").prev().find(".approve-checkmark").hide()
+            },
+            complete: function (xhr, status) {
+
+            }
+        }
+        )
+
+    });
+});
--- userprofile/urls.py
+++ userprofile/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('<int:pk>/', views.ProfileDetailView.as_view(), name='profile_by_id'),
     path('edit/', views.ProfileUpdateView.as_view(), name="edit_profile"),
     path('skills/', views.SelfSkillsView.as_view(), name="profile_skills"),
-    path('<int:pk>/skills/', views.SkillsView.as_view(), name="profile_skills_by_id")
+    path('<int:pk>/skills/', views.SkillsView.as_view(), name="profile_skills_by_id"),
+    path('<int:pk>/skills/approve/', views.ApproveSkillAPIView.as_view(), name="approve_skill_api_endpoint")
 ]

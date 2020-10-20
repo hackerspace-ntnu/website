3bd0ed735f3cbce158dfe47aa418e7b444dd74be
Markus <mwh@halvorsenfamilien.com>
2020-10-21 00:56:29 +0200

    Finish basic inventory
    Touches up the styling a bit and adds search and sorting functionality

--- inventory/models.py
+++ inventory/models.py
@@ -6,8 +6,10 @@ class Item(models.Model):
 
     name = models.CharField('Navn', max_length=100)
     stock = models.IntegerField('Lagerbeholdning')
-    description = models.CharField('Beskrivelse', max_length=200)
+    description = models.TextField('Beskrivelse', max_length=200, blank=True)
     image = models.ImageField('Bilde', blank=True)
+
+    views = models.IntegerField('Detaljsidevisninger', default=0, editable=True)
 
     def __str__(self):
         return self.name + ' (x' + str(self.stock) + ')'
@@ -15,5 +17,15 @@ class Item(models.Model):
     def in_stock(self):
         '''Whether or not the item is in stock'''
         return self.stock > 0
+
+    def popularity(self):
+        '''
+        Returns a measure of popularity for this item
+        
+        For now, this is just the amount of detail page views the item has gotten
+        In the future, this may be expanded to weigh other statistics, such as
+        how frequently the item is loaned.
+        '''
+        return self.views
     
 

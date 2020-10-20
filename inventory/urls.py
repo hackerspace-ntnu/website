8743bfecd7e2518b0c7ba3b0fd5d71a0fcce3eec
Markus Wang Halvorsen <markus@halvorsenfamilien.com>
2020-10-20 18:50:57 +0200

    Inventory list view

---
+++ inventory/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'inventory'
+
+urlpatterns = [
+    path('', views.InventoryView.as_view(), name='inventory'),
+    path('item/<int:pk>', views.ItemDetailView.as_view(), name='item'),
+]
--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Item
 
-# Create your views here.
+
+class InventoryView(ListView):
+    model = Item
+    paginate_by = 3
+    template_name = 'inventory/inventory.html'
+
+    context_object_name = 'items'
+
+    def get_queryset(self):
+        return Item.objects.all()
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'inventory/item_detail.html'

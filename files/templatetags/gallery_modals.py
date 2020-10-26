1a5a9512b1ee2fa478cda35f2ebda3297e5fcc4f
Markus Wang Halvorsen <markus@halvorsenfamilien.com>
2020-10-26 15:20:39 +0100

    Allow image uploads from the gallery admin view

--- files/templatetags/gallery_modals.py
+++ files/templatetags/gallery_modals.py
@@ -8,4 +8,8 @@ register = template.Library()
 def ImagePickModal(request):
     images = Image.objects.order_by('tags', '-time')
 
-    return {'UploadForm': ImageForm(prefix="img"), 'images': images}
+    return {'images': images}
+
+@register.inclusion_tag('files/_upload_modal.html')
+def ImageUploadModal(request):
+    return {'UploadForm': ImageForm(prefix="img")}
--- files/urls.py
+++ files/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
+app_name = 'files'
+
 urlpatterns = [
     path('images/', views.ImageListView.as_view(), name='images'),
     path('image-upload/', views.imageUpload, name='image-upload'),

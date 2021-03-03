a6c5d9cedc6678c0c74367c9ec43f454d2e715b7
Halvor Bjørstad <43417915+Riphiphip@users.noreply.github.com>
2021-03-03 19:50:17 +0100

    Basic app setup + HW view

--- watchlist/views.py
+++ watchlist/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class watchlistView(TemplateView):
+    template_name = 'watchlist/watchlist.html'

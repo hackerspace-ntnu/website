a5a1d8db44768e1c155ecb48b59a2952d5cc7fc8
Markus <mwh@halvorsenfamilien.com>
2021-03-24 19:57:04 +0100

    Add legend to watchlist

--- watchlist/views.py
+++ watchlist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .utils import get_shift_weekview_rows, get_shift_weekview_columns
+from userprofile.models import Skill, Category
 
 class watchlistView(TemplateView):
     template_name = 'watchlist/watchlist.html'
@@ -9,4 +10,12 @@ class watchlistView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["columns"] = get_shift_weekview_columns()
         context["rows"] = get_shift_weekview_rows()
+
+        skills = Skill.objects.all()
+        skill_cats = []
+        for skill in skills:
+            for cat in skill.categories.all():
+                if cat not in skill_cats:
+                    skill_cats.append(cat)
+        context["skill_categories"] = skill_cats
         return context

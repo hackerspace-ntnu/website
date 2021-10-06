8f7471e9859a3d6bf3de15d1d297f8c1094da702
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-10-06 19:39:49 +0200

    fix hiding irrelevant fields with js

--- news/static/news/js/edit_event.js
+++ news/static/news/js/edit_event.js
@@ -45,29 +45,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
 	// Vis og gjem deler som relateres til påmeldinger
-	var reg_box = document.getElementsByClassName('reg-box')[0];
-	var reg_check = document.getElementsByName('registration')[0]
-	var ext_reg = document.getElementsByClassName('ext-reg')[0];
+	const reg_boxes = document.getElementsByClassName('reg-box');
+	const reg_check = document.getElementsByName('registration')[0];
+	const ext_reg = document.getElementsByClassName('ext-reg')[0];
 
-	if(reg_check.checked) {
-		reg_box.classList.remove('hide');
-		ext_reg.classList.add('hide');
-	}
-	else {
-		reg_box.classList.add('hide');
-		ext_reg.classList.remove('hide');
-	}
-		
-	reg_check.onchange = function() {
-		if(this.checked) {
-			reg_box.classList.remove('hide');
+	const setRegistrationHideClasses = checkbox => {
+		if(checkbox.checked) {
+			for (const reg_box of reg_boxes) {
+				reg_box.classList.remove('hide')
+			}
 			ext_reg.classList.add('hide');
-		}
-		else {
-			reg_box.classList.add('hide');
+		} else {
+			for (const reg_box of reg_boxes) {
+				reg_box.classList.add('hide')
+			}
 			ext_reg.classList.remove('hide');
 			document.getElementById('id_external_registration').value = '';
 		}
-	};
+	}
 
+	setRegistrationHideClasses(reg_check);
+
+	reg_check.onchange = function() { setRegistrationHideClasses(this) };
 });

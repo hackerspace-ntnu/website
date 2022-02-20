5d2621b403297d6c5acb670bd618718d2be3ba64
Mathias Oterhals Myklebust <mathias@oterbust.no>
2022-02-20 23:32:17 +0100

    Refactor project archive

--- projectarchive/urls.py
+++ projectarchive/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = "projectarchive"
 urlpatterns = [
-    path("", views.ArticleListView.as_view(), name="Article_List"),
+    path("", views.ArticleListView.as_view(), name="all"),
     path("<int:pk>", views.ArticleView.as_view(), name="details"),
-    path("new", views.ArticleCreateView.as_view(), name="Article_Create"),
+    path("new", views.ArticleCreateView.as_view(), name="create"),
     path("<int:pk>/edit", views.ArticleUpdateView.as_view(), name="edit"),
     path("<int:pk>/delete", views.ArticleDeleteView.as_view(), name="delete"),
 ]

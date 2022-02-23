f0c0a550a89be2279d79e95634049403955f5939
Alida <hjelljordalida@gmail.com>
2022-02-23 19:02:20 +0100

    Reformated with black

--- applications/validators.py
+++ applications/validators.py
@@ -5,10 +5,10 @@ from django.utils.translation import ugettext_lazy as _
 def validate_phone_number(number):
     has_areacode = number[0] == "+" or number[:1] == "00"
     if len(number) != 8 and (not has_areacode or number[1:].isdigit()):
-            raise ValidationError(
-                _(
-                    "Nummeret må være 8 siffer eller starte med landskode på formen +47 eller 0047"
-                )
+        raise ValidationError(
+            _(
+                "Nummeret må være 8 siffer eller starte med landskode på formen +47 eller 0047"
             )
+        )
     if len(number) == 8 and not number.isdigit():
         raise ValidationError(_("Nummeret må være 8 siffer"))

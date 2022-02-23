c43030a1446ca1280d7645330e6c71624a3eaad0
Alida <hjelljordalida@gmail.com>
2022-02-23 19:10:51 +0100

    Fiksa logisk feil i valideringen

--- applications/validators.py
+++ applications/validators.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 def validate_phone_number(number):
     has_areacode = number[0] == "+" or number[:1] == "00"
-    if len(number) != 8 and (not has_areacode or number[1:].isdigit()):
+    if len(number) != 8 and (not has_areacode or not number[1:].isdigit()):
         raise ValidationError(
             _(
                 "Nummeret må være 8 siffer eller starte med landskode på formen +47 eller 0047"

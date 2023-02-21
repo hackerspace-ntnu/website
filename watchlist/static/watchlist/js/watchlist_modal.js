c960a44daeb04ff3f8b56b6e3e9c4441f3d9b84d
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2023-02-21 19:48:53 +0100

    Added a login alert text for the watchlist
    Co-authored-by: siljehtangen <siljehtangen@users.noreply.github.com>

--- watchlist/static/watchlist/js/watchlist_modal.js
+++ watchlist/static/watchlist/js/watchlist_modal.js
@@ -5,9 +5,10 @@ let modal_time = '';
 let modal_watchers = [];
 let modal_register = true;
 let modal_register_url = '';
+let has_perms = '';
+let close_button = '<a href="#!" class="modal-close btn hs-red">Lukk</a>';
 
-function updateModalInformation()
-{
+function updateModalInformation() {
     $('#registration-modal-day').text(weekdays[modal_day]);
     // Tiden til vakten inneholder i utgangspunktet en newline. Dette er vanligvis en syntaksfeil fordi vi ikke kan escape den
     // Så vi setter inn tiden med <br> for newlines i templaten, så fjerner vi den her for å få tidsrammen på en linje
@@ -17,40 +18,43 @@ function updateModalInformation()
     const watchlist_div = $('#registration-modal-watchlist');
     // Tøm div-en
     watchlist_div.empty();
-    if (modal_watchers.length > 0)
-    {
-        watchlist_div.append($('<h5>Vakthavende</h5>'));
-        // Legg til navnet på alle vakthavende
-        const watchlist_names = $('<p></p>');
-        for (let i = 0; i < modal_watchers.length; i++)
-        {
-            watchlist_names.append($('<span>' + modal_watchers[i] + '</span><br>'));
-        }
-        watchlist_div.append(watchlist_names);
-    }
-    else
-    {
-        watchlist_div.append($('<p>Ingen på vakt!</p>'));
-    }
 
     // Tøm diven for knappelering
     const footer_div = $('#registration-modal-footer');
     footer_div.empty();
+
     // legg til knapp for på-/avregistrering
     let change_registration_button = $('<a class="btn"></a>');
     change_registration_button.attr('href', modal_register_url);
-    if (modal_register === true)
-    {
+    if (modal_register === true) {
         change_registration_button.addClass('hs-green');
         change_registration_button.text('Registrer');
     }
-    else
-    {
+    else {
         change_registration_button.addClass('hs-red');
         change_registration_button.text('Avregistrer');
     }
-    footer_div.append(change_registration_button);
-    // og sleng med en avbryt-knapp
-    footer_div.append(' ');
-    footer_div.append($('<a href="#!" class="modal-close btn hs-red">Avbryt</a>'));
+
+    if (has_perms === "False") {
+        watchlist_div.append($('<p>DU MÅ LOGGE INN FOR Å REGISTRERE VAKT!</p>'));
+        footer_div.append($(close_button));
+    } else {
+        if (modal_watchers.length > 0) {
+            watchlist_div.append($('<h5>Vakthavende</h5>'));
+            // Legg til navnet på alle vakthavende
+            const watchlist_names = $('<p></p>');
+            for (let i = 0; i < modal_watchers.length; i++) {
+                watchlist_names.append($('<span>' + modal_watchers[i] + '</span><br>'));
+            }
+            watchlist_div.append(watchlist_names);
+        }
+        else {
+            watchlist_div.append($('<p>Ingen på vakt!</p>'));
+        }
+
+        footer_div.append(change_registration_button);
+        // og sleng med en avbryt-knapp
+        footer_div.append(' ');
+        footer_div.append($(close_button));
+    }
 }

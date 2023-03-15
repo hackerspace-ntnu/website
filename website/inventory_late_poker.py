042fa4b1128441232ee5d58846000cb789958c99
David Spilde <davidssp@stud.ntnu.no>
2023-03-15 12:46:54 +0100

    cold feet

--- website/inventory_late_poker.py
+++ website/inventory_late_poker.py
@@ -30,35 +30,3 @@ def late_loan_retrieval_poker():
             fail_silently=False,
         )
 
-
-def amogus():
-    # Create message object instance
-    msg = MIMEMultipart()
-
-    # Define email parameters
-    sender_email = settings.DEFAULT_FROM_MAIL
-    recipient_email = "alexamol@stud.ntnu.no"
-    subject = "Du har vært litt for sussy!"
-
-    # Attach the image to the email
-    with open("static/img/misc/easteregg.png", "rb") as f:
-        img = MIMEImage(f.read())
-        img.add_header("Content-Disposition", "attachment", filename="cat_picture.jpg")
-        msg.attach(img)
-
-    # Define message headers
-    msg["From"] = sender_email
-    msg["To"] = recipient_email
-    msg["Subject"] = subject
-
-    # Define SMTP server details and login credentials
-    smtp_server = settings.EMAIL_HOST
-    smtp_port = settings.EMAIL_PORT
-    smtp_username = "hacker_space@yahoo.com"
-    smtp_password = "QckNmieE6Lahhcm"
-
-    # Create a SMTP session and send the email
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        server.starttls()
-        server.login(smtp_username, smtp_password)
-        server.sendmail(sender_email, recipient_email, msg.as_string())

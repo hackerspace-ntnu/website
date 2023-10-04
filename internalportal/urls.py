136b1ff74cf5cc311c985d95fc6e9dd1e9ffe7e9
Njål Telstø <njal_telsto@hotmail.com>
2023-10-04 18:16:04 +0200

    Listing group applications (WIP)

--- internalportal/urls.py
+++ internalportal/urls.py
@@ -6,4 +6,5 @@ app_name = "internalportal"
 
 urlpatterns = [
     path("", views.InternalPortalView.as_view(), name="internalportal"),
+    path("applications/", views.ApplicationsView.as_view(), name="applications"),
 ]

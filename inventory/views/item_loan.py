9db7aee5b6cb8c711ed3ff3f2f03edbdb942867d
erlendska <erlend.ulvund.skaarberg@gmail.com>
2023-10-11 18:25:58 +0200

    karl

--- inventory/views/item_loan.py
+++ inventory/views/item_loan.py
@@ -18,6 +18,27 @@ from inventory.models.item import Item
 from inventory.models.item_loan import ItemLoan
 from userprofile.models import Profile
 
+# from typing import List
+
+
+""" @atomic
+def run_script(list_items: List[dict]):
+    items: List[Item] = []
+    for item in list_items:
+        item = Item(name=item["name"], stock=item["stock"])
+        Item.objects.update_or_create(
+            name=item["name"], defaults={"stock": item["stock"]}
+        ) """
+
+
+# HTML template for å laste opp csv
+# Link til html siden
+# Form for å laste opp csvfil
+# View tar imot form og verdier fra csv filen
+# Sender verdier til run_script
+# View sender success tilbake
+# Behandle at idioter sender feil data (som de absolutt kommer til å gjøre)
+
 
 class ItemLoanListView(PermissionRequiredMixin, ListView):
     """View for viewing all loan applications"""

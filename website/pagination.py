5b16935ecba54270e8a17e510343980ee14c3765
Carl Gützkow <70779496+CJGutz@users.noreply.github.com>
2024-02-07 20:30:38 +0100

    switch to standard result set pagination

--- website/pagination.py
+++ website/pagination.py
@@ -1,7 +1,6 @@
-from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import LimitOffsetPagination
 
 
-class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = "page_size"
-    max_page_size = 1000
+class StandardResultsSetPagination(LimitOffsetPagination):
+    default_limit = 50
+    max_limit = 500

hackerspace-ntnu/website

commit 85c78ca6c9e49a098ff7a5429c6256038db9df5e

change to path

applications/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path
 from django.views.generic import TemplateView
 
 from applications import views
@@ -6,10 +6,10 @@ from applications import views
 app_name = "application"
 
 urlpatterns = [
-    url(r"^$", views.ApplicationInfoView.as_view(), name="application_info"),
-    url(r"^application", views.ApplicationView.as_view(), name="application_form"),
-    url(
-        r"^success",
+    path("", views.ApplicationInfoView.as_view(), name="application_info"),
+    path("application", views.ApplicationView.as_view(), name="application_form"),
+    path(
+        "success",
         TemplateView.as_view(template_name="applications/application_success.html"),
         name="application_success",
     ),

news/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = "news"
 urlpatterns = [
-    url(r"^$", views.ArticleListView.as_view(), name="all"),
-    url(r"^(?P<pk>[0-9]+)/$", views.ArticleView.as_view(), name="details"),
-    url(r"^(?P<pk>[0-9]+)/edit", views.ArticleUpdateView.as_view(), name="edit"),
-    url(r"^new", views.ArticleCreateView.as_view(), name="new"),
-    url(r"^(?P<pk>[0-9]+)/delete", views.ArticleDeleteView.as_view(), name="delete"),
+    path("", views.ArticleListView.as_view(), name="all"),
+    path("<int:pk>", views.ArticleView.as_view(), name="details"),
+    path("<int:pk>/edit", views.ArticleUpdateView.as_view(), name="edit"),
+    path("new", views.ArticleCreateView.as_view(), name="new"),
+    path("<int:pk>/delete", views.ArticleDeleteView.as_view(), name="delete"),
 ]

commit 0ba27aa8155d1029edcee6e0895ffef0109bf8c3

Made URLs end with a trailing slash
Reasons:
* Many/most of the website's views use a trailing slash in their URLs,
  so this will make it consistent and (hopefully) establish an internal
  convention
* To my knowledge, it's conventional for (Django) websites to use a
  trailing slash in URLs (except for URLs to files, of course)
* Django redirects URLs *missing* a trailing slash to the same URL
  *with* a trailing slash by default, but not vice versa (see
  https://docs.djangoproject.com/en/stable/ref/settings/#append-slash)
* This fixes a 404 bug introduced in
  85c78ca6c9e49a098ff7a5429c6256038db9df5e - which removed the trailing
  slash from the "news:details" path

applications/urls.py
@@ -7,9 +7,9 @@ app_name = "application"
 
 urlpatterns = [
     path("", views.ApplicationInfoView.as_view(), name="application_info"),
-    path("application", views.ApplicationView.as_view(), name="application_form"),
+    path("application/", views.ApplicationView.as_view(), name="application_form"),
     path(
-        "success",
+        "success/",
         TemplateView.as_view(template_name="applications/application_success.html"),
         name="application_success",
     ),

authentication/urls.py
@@ -5,5 +5,5 @@ from authentication.views import LogoutView
 app_name = "auth"
 
 urlpatterns = [
-    path("logout", LogoutView.as_view(), name="logout"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]

door/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.DoorView.as_view(), name="door_post"),
-    path("json", views.DoorJsonView.as_view(), name="get_json"),
+    path("json/", views.DoorJsonView.as_view(), name="get_json"),
 ]

files/templates/files/_gallery_include.html
@@ -8,7 +8,7 @@
 
 <div class="row">
     {% if form.thumbnail.value and form.thumbnail.value != "None" %}
-        <div id="thumb-preview" class="card small thumb-preview col s12" style="background-image: url('/files/image-view/{{ form.thumbnail.value }}'); background-repeat:no-repeat; background-size: cover;">
+        <div id="thumb-preview" class="card small thumb-preview col s12" style="background-image: url('/files/image-view/{{ form.thumbnail.value }}/'); background-repeat:no-repeat; background-size: cover;">
     {% else %}
         <div id="thumb-preview" class="card small thumb-preview col s12" style="background-image: none; background-repeat:no-repeat; background-size:cover;">
     {% endif %}
@@ -25,7 +25,7 @@
         if(e.target.innerText === "{% trans 'VELG' %}") { // This is really funny to me
             var selectedImgId = e.target.attributes.value.value;
             thumbnail_input.value = selectedImgId;
-            thumb_preview.style.backgroundImage = "url('/files/image-view/" + selectedImgId;
+            thumb_preview.style.backgroundImage = `url('/files/image-view/${selectedImgId}/`;
             M.toast({html: '{% trans "Bildet er valgt" %}'});
             M.Modal.getInstance(thumbModal).close();
         }

files/urls.py
@@ -7,6 +7,8 @@ app_name = "files"
 urlpatterns = [
     path("images/", views.ImageListView.as_view(), name="images"),
     path("image-upload/", views.imageUpload, name="image-upload"),
-    path("image-delete/<int:pk>", views.ImageDeleteView.as_view(), name="image-delete"),
-    path("image-view/<int:pk>", views.ImageView.as_view(), name="image-detail"),
+    path(
+        "image-delete/<int:pk>/", views.ImageDeleteView.as_view(), name="image-delete"
+    ),
+    path("image-view/<int:pk>/", views.ImageView.as_view(), name="image-detail"),
 ]

internalportal/urls.py
@@ -7,24 +7,24 @@ app_name = "internalportal"
 urlpatterns = [
     path("", views.InternalPortalView.as_view(), name="internalportal"),
     path("applications/", views.ApplicationsView.as_view(), name="applications"),
-    path("applications/<int:pk>", views.ApplicationView.as_view(), name="application"),
+    path("applications/<int:pk>/", views.ApplicationView.as_view(), name="application"),
     path(
-        "applications/delete/<int:pk>",
+        "applications/delete/<int:pk>/",
         views.ApplicationRemoveView.as_view(),
         name="delete_application",
     ),
     path(
-        "applications/next-group/<int:pk>",
+        "applications/next-group/<int:pk>/",
         views.ApplicationNextGroupView.as_view(),
         name="next_group",
     ),
     path(
-        "applications/<int:pk>/approve",
+        "applications/<int:pk>/approve/",
         views.ApplicationApproveView.as_view(),
         name="approve_application",
     ),
     path(
-        "applications/<int:pk>/interview-email",
+        "applications/<int:pk>/interview-email/",
         views.ApplicationInterviewEmailView.as_view(),
         name="interview_email",
     ),

inventory/urls.py
@@ -29,34 +29,36 @@ app_name = "inventory"
 
 urlpatterns = [
     path("", InventoryListView.as_view(), name="inventory"),
-    path("item/<int:pk>", ItemDetailView.as_view(), name="item"),
-    path("new", ItemCreateView.as_view(), name="new"),
-    path("edit/<int:pk>", ItemUpdateView.as_view(), name="edit"),
-    path("delete/<int:pk>", ItemDeleteView.as_view(), name="delete"),
+    path("item/<int:pk>/", ItemDetailView.as_view(), name="item"),
+    path("new/", ItemCreateView.as_view(), name="new"),
+    path("edit/<int:pk>/", ItemUpdateView.as_view(), name="edit"),
+    path("delete/<int:pk>/", ItemDeleteView.as_view(), name="delete"),
     path("loans/", ItemLoanListView.as_view(), name="loans"),
-    path("loans/<int:pk>", ItemLoanDetailView.as_view(), name="loan_application"),
+    path("loans/<int:pk>/", ItemLoanDetailView.as_view(), name="loan_application"),
     path(
-        "loans/approve/<int:pk>",
+        "loans/approve/<int:pk>/",
         ItemLoanApproveView.as_view(),
         name="loan_approve",
     ),
-    path("loans/deny/<int:pk>", ItemLoanDeclineView.as_view(), name="loan_deny"),
+    path("loans/deny/<int:pk>/", ItemLoanDeclineView.as_view(), name="loan_deny"),
     path(
-        "loans/returned/<int:pk>",
+        "loans/returned/<int:pk>/",
         ItemLoanReturnedView.as_view(),
         name="loan_returned",
     ),
     path(
-        "loans/apply/<int:pk>",
+        "loans/apply/<int:pk>/",
         ItemLoanApplicationView.as_view(),
         name="loan_apply",
     ),
-    path("equipment", EquipmentListView.as_view(), name="equipment"),
-    path("equipment/item/<int:pk>", EquipmentView.as_view(), name="equipment_detail"),
-    path("equipment/new", EquipmentCreateView.as_view(), name="equipment_new"),
-    path("equipment/edit/<int:pk>", EquipmentEditView.as_view(), name="equipment_edit"),
+    path("equipment/", EquipmentListView.as_view(), name="equipment"),
+    path("equipment/item/<int:pk>/", EquipmentView.as_view(), name="equipment_detail"),
+    path("equipment/new/", EquipmentCreateView.as_view(), name="equipment_new"),
     path(
-        "equipment/delete/<int:pk>",
+        "equipment/edit/<int:pk>/", EquipmentEditView.as_view(), name="equipment_edit"
+    ),
+    path(
+        "equipment/delete/<int:pk>/",
         EquipmentDeleteView.as_view(),
         name="equipment_delete",
     ),

news/event_urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("<int:pk>/edit/", views.EventUpdateView.as_view(), name="edit"),
     path("<int:pk>/attended/", views.EventAttendeeEditView.as_view(), name="attended"),
     path("<int:pk>/skills/", views.EventAttendeeSkillsView.as_view(), name="skills"),
-    path("new", views.EventCreateView.as_view(), name="new"),
+    path("new/", views.EventCreateView.as_view(), name="new"),
     path("<int:pk>/delete/", views.EventDeleteView.as_view(), name="delete"),
     path("<int:event_id>/register/", views.register_on_event, name="register"),
     path("feed.ics", HSEventFeed()),

news/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "news"
 urlpatterns = [
     path("", views.ArticleListView.as_view(), name="all"),
-    path("<int:pk>", views.ArticleView.as_view(), name="details"),
-    path("<int:pk>/edit", views.ArticleUpdateView.as_view(), name="edit"),
-    path("new", views.ArticleCreateView.as_view(), name="new"),
-    path("<int:pk>/delete", views.ArticleDeleteView.as_view(), name="delete"),
+    path("<int:pk>/", views.ArticleView.as_view(), name="details"),
+    path("<int:pk>/edit/", views.ArticleUpdateView.as_view(), name="edit"),
+    path("new/", views.ArticleCreateView.as_view(), name="new"),
+    path("<int:pk>/delete/", views.ArticleDeleteView.as_view(), name="delete"),
 ]

projectarchive/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = "projectarchive"
 urlpatterns = [
-    path(r"", views.ArticleListView.as_view(), name="Article_List"),
-    path("<int:pk>", views.ArticleView.as_view(), name="details"),
-    path(r"new", views.ArticleCreateView.as_view(), name="Article_Create"),
-    path("<int:pk>/edit", views.ArticleUpdateView.as_view(), name="edit"),
-    path("<int:pk>/delete", views.ArticleDeleteView.as_view(), name="delete"),
+    path("", views.ArticleListView.as_view(), name="Article_List"),
+    path("<int:pk>/", views.ArticleView.as_view(), name="details"),
+    path("new/", views.ArticleCreateView.as_view(), name="Article_Create"),
+    path("<int:pk>/edit/", views.ArticleUpdateView.as_view(), name="edit"),
+    path("<int:pk>/delete/", views.ArticleDeleteView.as_view(), name="delete"),
 ]

userprofile/urls.py
@@ -9,17 +9,17 @@ urlpatterns = [
     path("edit/", views.ProfileUpdateView.as_view(), name="edit_profile"),
     path("skills/", views.SelfSkillsView.as_view(), name="profile_skills"),
     path(
-        "skills/<int:skill_pk>", views.SelfSkillsView.as_view(), name="profile_skills"
+        "skills/<int:skill_pk>/", views.SelfSkillsView.as_view(), name="profile_skills"
     ),
     path(
-        "skills/category/<int:pk>",
+        "skills/category/<int:pk>/",
         views.SkillsCategoryView.as_view(),
         name="profile_skills_category",
     ),
     path("<int:pk>/", views.ProfileDetailView.as_view(), name="profile_by_id"),
     path("<int:pk>/skills/", views.SkillsView.as_view(), name="profile_skills_by_id"),
     path(
-        "<int:pk>/skills/<int:skill_pk>",
+        "<int:pk>/skills/<int:skill_pk>/",
         views.SkillsView.as_view(),
         name="profile_skills_by_id",
     ),

watchlist/urls.py
@@ -6,11 +6,11 @@ app_name = "watchlist"
 
 urlpatterns = [
     path("", views.watchlistView.as_view(), name="vaktliste"),
-    path("register/<int:pk>", views.WatchListRegisterView.as_view(), name="register"),
+    path("register/<int:pk>/", views.WatchListRegisterView.as_view(), name="register"),
     path(
-        "unregister/<int:pk>",
+        "unregister/<int:pk>/",
         views.WatchListUnregisterView.as_view(),
         name="unregister",
     ),
-    path("reset", views.WatchListResetView.as_view(), name="reset"),
+    path("reset/", views.WatchListResetView.as_view(), name="reset"),
 ]